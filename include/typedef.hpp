05d0a35f4327db7c1090a838b01b2168d6c21d9b
Alexander Van Craen <Alexander.Van-Craen@ipvs.uni-stuttgart.de>
2018-12-19 09:59:28 +0100

    typedef kommentiert

--- include/typedef.hpp
+++ include/typedef.hpp
@@ -1,5 +1,7 @@
 #pragma once
-typedef double real_t;
+typedef double real_t;                              //TODO: float Fehler finden
 
-static const unsigned CUDABLOCK_SIZE = 16;
-static const unsigned BLOCKING_SIZE_THREAD = 6;
+static const unsigned CUDABLOCK_SIZE = 16;          // minimal 1
+static const unsigned BLOCKING_SIZE_THREAD = 6;     // minimal 1 maximal CUDABLOCK_SIZE/2 //TODO: beschränkung für CUDABLOCK_SIZE/2 entfernen
+
+
