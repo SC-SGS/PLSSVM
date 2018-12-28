982335c8ea3fb9163c4e0c51d6ea8bee8d89cc96
Alexander Van Craen <Alexander.Van-Craen@ipvs.uni-stuttgart.de>
2018-12-28 11:30:12 +0100

    distribution header

---
+++ include/distribution.hpp
@@ -0,0 +1,31 @@
+#pragma once
+#include <vector>
+#include <numeric>
+#include <functional>
+#include "typedef.hpp"
+
+class distribution{
+    public:
+    distribution(const size_t number_devices, const size_t number_lines, const std::vector<real_t> parameter){
+        if(number_devices != parameter.size()) throw "Distribution error!";
+        real_t sum = std::accumulate(parameter.begin(), parameter.end(), 0);
+        for(real_t value :parameter){
+            distr.push_back(value / sum * number_lines);
+        }
+
+        distr[number_devices] += std::accumulate(distr.begin(), distr.end(), number_lines, std::minus<real_t>());
+    }
+
+//TODO: optional
+    // distribution(const size_t number_devices, const size_t number_lines){
+        
+    // }
+    
+    bool isValid(size_t number_devices, std::vector<real_t> parameter){
+        return (number_devices <= parameter.size() && std::accumulate(parameter.begin(), parameter.end(), 0) <= 1.0);
+    }
+
+    std::vector<real_t>distr;
+
+
+};
