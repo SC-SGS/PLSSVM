0ddc571fddeb8f60dcbb5572b138c112b674ed28
Alexander Van Craen <Alexander.Van-Craen@ipvs.uni-stuttgart.de>
2019-04-14 15:03:10 +0200

    CUDA anpassung

--- include/typedef.hpp
+++ include/typedef.hpp
@@ -1,8 +1,8 @@
 #pragma once
-typedef double real_t;                              //TODO: float Fehler finden
+typedef float real_t;                              //TODO: float Fehler finden
 
 static const unsigned CUDABLOCK_SIZE = 6;          // minimal 1
-static const unsigned THREADBLOCK_SIZE = CUDABLOCK_SIZE;
-// static const unsigned BLOCKING_SIZE_THREAD = ;    
+static const unsigned THREADBLOCK_SIZE = CUDABLOCK_SIZE; //TODO: immer nur 1 mal
 static const unsigned INTERNALBLOCK_SIZE = 8;       // minimal 1
+static const unsigned BLOCKING_SIZE_THREAD = INTERNALBLOCK_SIZE;    
 
