37f1fc233afc38b14f7d44aca871ca40d8f6d653
Alexander Van Craen <Alexander.Van-Craen@ipvs.uni-stuttgart.de>
2021-06-28 17:28:26 +0200

    fix tests add namespace add missing CMakeLists

--- tests/mocks/CSVM.hpp
+++ tests/mocks/CSVM.hpp
@@ -1,11 +1,11 @@
 #pragma once
-#include "../include/CSVM.hpp"
-#include "typedef.hpp"
+#include "plssvm/CSVM.hpp"
+#include "plssvm/typedef.hpp"
 #include "gmock/gmock.h"
 
-class MockCSVM : public CSVM {
+class MockCSVM : public plssvm::CSVM {
   public:
-    MockCSVM(real_t cost_, real_t epsilon_, unsigned kernel_, real_t degree_, real_t gamma_, real_t coef0_, bool info_) : CSVM(cost_, epsilon_, kernel_, degree_, gamma_, coef0_, info_) {}
+    MockCSVM(real_t cost_, real_t epsilon_, unsigned kernel_, real_t degree_, real_t gamma_, real_t coef0_, bool info_) : plssvm::CSVM(cost_, epsilon_, kernel_, degree_, gamma_, coef0_, info_) {}
     MOCK_METHOD(void, load_w, (), (override));
     MOCK_METHOD(std::vector<real_t>, predict, (real_t *, int, int), (override));
     MOCK_METHOD(void, learn, (), (override));
