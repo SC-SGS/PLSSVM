d617bf070215aa762d0bee0dcabe1b966618933e
Marcel Breyer <marcel.breyer@ipvs.uni-stuttgart.de>
2021-06-29 10:30:41 +0200

    Fix spelling mistake.

--- include/plssvm/exceptions.hpp
+++ include/plssvm/exceptions.hpp
@@ -1,28 +1,27 @@
 #pragma once
 
 #include <stdexcept>
-#include <string_view>
 
 namespace plssvm {
 
  class exception : public std::runtime_error {
   public:
-    explicit exception(const std::string_view msg) : std::runtime_error{msg.data()} { }
+    explicit exception(const std::string& msg) : std::runtime_error{msg} { }
  };
 
  class file_not_found_exception : public exception {
   public:
-   explicit file_not_found_exception(const std::string_view msg) : exception{msg} { }
+   explicit file_not_found_exception(const std::string& msg) : exception{msg} { }
  };
 
  class invalid_file_format_exception : public exception {
   public:
-   explicit invalid_file_format_exception(const std::string_view msg) : excpetion{msg} { }
+   explicit invalid_file_format_exception(const std::string& msg) : exception{msg} { }
  };
 
  class unsupported_backend_exception : public exception {
   public:
-   explicit unsupported_backend_exception(const std::string_view msg) : exception{msg} { }
+   explicit unsupported_backend_exception(const std::string& msg) : exception{msg} { }
  };
 
 }
