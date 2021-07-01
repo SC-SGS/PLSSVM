f585115c8469ec9270c958fe0de6d9ddda58ee9f
Marcel Breyer <marcel.breyer@ipvs.uni-stuttgart.de>
2021-07-01 14:33:24 +0200

    Add correct (per OpenMP standard) local exception handling.

--- src/plssvm/IO.cpp
+++ src/plssvm/IO.cpp
@@ -35,39 +35,58 @@ void CSVM::libsvmParser(const std::string_view filename) {
     data.resize(data_lines.size());
 
     std::size_t max_size = 0;
+    std::exception_ptr parallel_exception;
 
-    #pragma omp parallel for reduction(max:max_size)
-    for (std::size_t i = 0; i < data.size(); ++i) {
-        std::string_view line = data_lines[i];
+    #pragma omp parallel
+    {
+        #pragma omp for reduction(max: max_size)
+        for (std::size_t i = 0; i < data.size(); ++i) {
+            #pragma omp cancellation point for
+            try {
+                std::string_view line = data_lines[i];
 
-        // get class
-        std::size_t pos = line.find_first_of(' ');
-        value[i] = util::convert_to<real_t, invalid_file_format_exception>(line.substr(0, pos)) > real_t{0.0} ? 1 : -1;
-        // value[i] = std::copysign(1.0, util::convert_to<real_t>(line.substr(0, pos)));
+                // get class
+                std::size_t pos = line.find_first_of(' ');
+                value[i] = util::convert_to<real_t, invalid_file_format_exception>(line.substr(0, pos)) > real_t{0.0} ? 1 : -1;
+                // value[i] = std::copysign(1.0, util::convert_to<real_t>(line.substr(0, pos)));
 
-        // get data
-        std::vector<real_t> vline(max_size);
-        while (true) {
-            std::size_t next_pos = line.find_first_of(':', pos);
-            // no further data points
-            if (next_pos == std::string_view::npos) {
-                break;
+                // get data
+                std::vector<real_t> vline(max_size);
+                while (true) {
+                    std::size_t next_pos = line.find_first_of(':', pos);
+                    // no further data points
+                    if (next_pos == std::string_view::npos) {
+                        break;
+                    }
+
+                    // get index
+                    const auto index = util::convert_to<unsigned long, invalid_file_format_exception>(line.substr(pos, next_pos - pos));
+                    if (index >= vline.size()) {
+                        vline.resize(index + 1);
+                    }
+                    pos = next_pos + 1;
+
+                    // get value
+                    next_pos = line.find_first_of(' ', pos);
+                    vline[index] = util::convert_to<real_t, invalid_file_format_exception>(line.substr(pos, next_pos - pos));
+                    pos = next_pos;
+                }
+                max_size = std::max(max_size, vline.size());
+                data[i] = std::move(vline);
+            } catch (const std::exception &e) {
+                // catch first exception and store it
+                if (!parallel_exception) {
+                    parallel_exception = std::current_exception();
+                }
+                // cancel parallel execution, needs env variable OMP_CANCELLATION=true
+                #pragma omp cancel for
             }
-
-            // get index
-            const auto index = util::convert_to<unsigned long, invalid_file_format_exception>(line.substr(pos, next_pos - pos));
-            if (index >= vline.size()) {
-                vline.resize(index + 1);
-            }
-            pos = next_pos + 1;
-
-            // get value
-            next_pos = line.find_first_of(' ', pos);
-            vline[index] = util::convert_to<real_t, invalid_file_format_exception>(line.substr(pos, next_pos - pos));
-            pos = next_pos;
         }
-        max_size = std::max(max_size, vline.size());
-        data[i] = std::move(vline);
+    }
+
+    // rethrow if an exception occurred inside the parallel region
+    if (parallel_exception) {
+        std::rethrow_exception(parallel_exception);
     }
 
     #pragma omp parallel for
@@ -81,7 +100,7 @@ void CSVM::libsvmParser(const std::string_view filename) {
 
     // no features were parsed -> invalid file
     if (num_features == 0) {
-        throw invalid_file_format_exception{fmt::format("Can't parse file '{}'!", filename)};
+        throw std::runtime_error{fmt::format("Can't parse file '{}'!", filename)};
     }
 
     // update gamma
@@ -157,63 +176,84 @@ void CSVM::arffParser(const std::string_view filename) {
         data[i].resize(max_size - 1);
     }
 
-    #pragma omp parallel for
-    for (std::size_t i = 0; i < data.size(); ++i) {
-        std::string_view line{util::trim_left(data_lines[i])};
+    std::exception_ptr parallel_exception;
 
-        // parse sparse or dense data point definition
-        if (util::starts_with(line, '{')) {
-            // missing closing }
-            if (!util::ends_with(line, '}')) {
-                throw invalid_file_format_exception{"Missing closing '}' for sparse data point description!"};
-            }
-            // sparse line
-            bool is_class_set = false;
-            std::size_t pos = 1;
-            while (true) {
-                std::size_t next_pos = line.find_first_of(' ', pos);
-                // no further data points
-                if (next_pos == std::string_view::npos) {
-                    break;
-                }
+    #pragma omp parallel
+    {
+        #pragma omp for
+        for (std::size_t i = 0; i < data.size(); ++i) {
+            #pragma omp cancellation point for
+            try {
 
-                // get index
-                const auto index = util::convert_to<unsigned long, invalid_file_format_exception>(line.substr(pos, next_pos - pos));
-                pos = next_pos + 1;
+                std::string_view line{util::trim_left(data_lines[i])};
 
-                // get value
-                next_pos = line.find_first_of(",}", pos);
+                // parse sparse or dense data point definition
+                if (util::starts_with(line, '{')) {
+                    // missing closing }
+                    if (!util::ends_with(line, '}')) {
+                        throw invalid_file_format_exception{"Missing closing '}' for sparse data point description!"};
+                    }
+                    // sparse line
+                    bool is_class_set = false;
+                    std::size_t pos = 1;
+                    while (true) {
+                        std::size_t next_pos = line.find_first_of(' ', pos);
+                        // no further data points
+                        if (next_pos == std::string_view::npos) {
+                            break;
+                        }
 
-                // write parsed value depending on the index
-                if (index == max_size - 1) {
-                    is_class_set = true;
-                    value[i] = util::convert_to<real_t, invalid_file_format_exception>(line.substr(pos)) > real_t{0.0} ? 1 : -1;
+                        // get index
+                        const auto index = util::convert_to<unsigned long, invalid_file_format_exception>(line.substr(pos, next_pos - pos));
+                        pos = next_pos + 1;
+
+                        // get value
+                        next_pos = line.find_first_of(",}", pos);
+
+                        // write parsed value depending on the index
+                        if (index == max_size - 1) {
+                            is_class_set = true;
+                            value[i] = util::convert_to<real_t, invalid_file_format_exception>(line.substr(pos)) > real_t{0.0} ? 1 : -1;
+                        } else {
+                            data[i][index] = util::convert_to<real_t, invalid_file_format_exception>(line.substr(pos, next_pos - pos));
+                        }
+
+                        // remove already processes part of the line
+                        line.remove_prefix(next_pos + 1);
+                        line = util::trim_left(line);
+                        pos = 0;
+                    }
+                    // no class label found
+                    if (!is_class_set) {
+                        throw invalid_file_format_exception{"Missing class for data point!"};
+                    }
                 } else {
-                    data[i][index] = util::convert_to<real_t, invalid_file_format_exception>(line.substr(pos, next_pos - pos));
+                    // dense line
+                    std::size_t pos = 0;
+                    for (std::size_t j = 0; j < max_size - 1; ++j) {
+                        std::size_t next_pos = line.find_first_of(',', pos);
+                        if (next_pos == std::string_view::npos) {
+                            throw invalid_file_format_exception{fmt::format("Invalid number of features! Found {} but should be {}.", j, max_size - 1)};
+                        }
+                        data[i][j] = util::convert_to<real_t, invalid_file_format_exception>(line.substr(pos, next_pos - pos));
+                        pos = next_pos + 1;
+                    }
+                    value[i] = util::convert_to<real_t, invalid_file_format_exception>(line.substr(pos)) > real_t{0.0} ? 1 : -1;
                 }
-
-                // remove already processes part of the line
-                line.remove_prefix(next_pos + 1);
-                line = util::trim_left(line);
-                pos = 0;
-            }
-            // no class label found
-            if (!is_class_set) {
-                throw invalid_file_format_exception{"Missing class for data point!"};
-            }
-        } else {
-            // dense line
-            std::size_t pos = 0;
-            for (std::size_t j = 0; j < max_size - 1; ++j) {
-                std::size_t next_pos = line.find_first_of(',', pos);
-                if (next_pos == std::string_view::npos) {
-                    throw invalid_file_format_exception{fmt::format("Invalid number of features! Found {} but should be {}.", j, max_size - 1)};
+            } catch (const std::exception &e) {
+                // catch first exception and store it
+                if (!parallel_exception) {
+                    parallel_exception = std::current_exception();
                 }
-                data[i][j] = util::convert_to<real_t, invalid_file_format_exception>(line.substr(pos, next_pos - pos));
-                pos = next_pos + 1;
+                // cancel parallel execution, needs env variable OMP_CANCELLATION=true
+                #pragma omp cancel for
             }
-            value[i] = util::convert_to<real_t, invalid_file_format_exception>(line.substr(pos)) > real_t{0.0} ? 1 : -1;
         }
+    }
+
+    // rethrow if an exception occurred inside the parallel region
+    if (parallel_exception) {
+        std::rethrow_exception(parallel_exception);
     }
 
     // update values
