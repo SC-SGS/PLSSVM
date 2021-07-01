f841f6c71f0ce034710d0e2c4ba83c66df008fe5
Marcel Breyer <marcel.breyer@ipvs.uni-stuttgart.de>
2021-07-01 17:56:43 +0200

    Update distribution class.

--- include/plssvm/distribution.hpp
+++ include/plssvm/distribution.hpp
@@ -1,7 +1,12 @@
 #pragma once
+
+#include <plssvm/typedef.hpp>
+#include <plssvm/exceptions.hpp>
+
+#include <fmt/core.h>
+
 #include <functional>
 #include <numeric>
-#include <plssvm/typedef.hpp>
 #include <vector>
 
 namespace plssvm {
@@ -10,23 +15,24 @@ class distribution {
   public:
     distribution() = default;
 
-    distribution(const size_t number_devices, const size_t number_lines, const std::vector<real_t> parameter) {
-        if (number_devices != parameter.size())
-            throw std::runtime_error("Distribution error!");
-        real_t sum = std::accumulate(parameter.begin(), parameter.end(), 0.0);
-        for (real_t value : parameter) {
-            distr.push_back(value / sum * number_lines);
+    distribution(const std::size_t number_devices, const std::size_t number_lines, const std::vector<real_t>& parameter) {
+        if (number_devices != parameter.size()) {
+            throw distribution_exception{fmt::format("Number of devices and parameters mismatch!: {} != {}", number_devices, parameter.size())};
         }
-        distr[number_devices - 1] += std::accumulate(distr.begin(), distr.end(), number_lines, std::minus<int>());
+        const real_t sum = std::accumulate(parameter.cbegin(), parameter.cend(), real_t{0.0});
+        for (const real_t value : parameter) {
+            distr.push_back(static_cast<int>(value / sum * static_cast<real_t>(number_lines)));
+        }
+        distr[number_devices - 1] += std::accumulate(distr.cbegin(), distr.cend(), static_cast<int>(number_lines), std::minus<>());
     }
 
-    //TODO: optional
-    // distribution(const size_t number_devices, const size_t number_lines){
-
+    // TODO: optional implement
+    // distribution(const std::size_t number_devices, const std::size_t number_lines) {
+    //
     // }
 
-    bool isValid(size_t number_devices, std::vector<real_t> parameter) {
-        return (number_devices <= parameter.size() && std::accumulate(parameter.begin(), parameter.end(), 0) <= 1.0);
+    [[nodiscard]] static bool isValid(const std::size_t number_devices, const std::vector<real_t>& parameter) {
+        return number_devices <= parameter.size() && std::accumulate(parameter.cbegin(), parameter.cend(), real_t{0.0}) <= 1.0;
     }
 
     std::vector<int> distr;
