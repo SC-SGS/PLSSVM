43f44f67b96032f19f6e03170e821b7a7b4fe49f
Marcel Breyer <marcel.breyer@ipvs.uni-stuttgart.de>
2021-07-02 12:22:22 +0200

    Document kernel type enum class and change conversion to string from fmt formatter to operator<< overload.

--- include/plssvm/kernel_types.hpp
+++ include/plssvm/kernel_types.hpp
@@ -1,36 +1,50 @@
+/**
+ * @file
+ * @author Alexander Van Craen
+ * @author Marcel Breyer
+ * @copyright
+ *
+ * @brief Defines all available kernel types.
+ */
+
 #pragma once
 
-#include <fmt/format.h>
+#include <fmt/ostream.h>
+
+#include <ostream>
 #include <string_view>
 
 namespace plssvm {
 
-// define possible kernel types
+/**
+ * @brief Enum class for the different kernel types.
+ */
 enum class kernel_type {
+    /**  \f$\vec{u} \cdot \vec{v}\f$ */
     linear = 0,
-    polynomial = 1,  // (gamma*u'*v + coef0)^degree
-    rbf = 2          // exp(-gamma*|u-v|^2)
+    /** \f$(gamma \cdot \vec{u} \cdot \vec{v} + coef0)^{degree}\f$ */
+    polynomial = 1,
+    /** \f$e^{(-gamma \cdot |\vec{u} - \vec{v}|^2)}\f$ */
+    rbf = 2
 };
+
+/**
+ * @brief Output-operator overload for convenient printing of the kernel type @p kernel.
+ * @param[inout] out the output-stream to write the kernel type to
+ * @param[in] kernel the kernel type
+ * @return the output-stream
+ */
+inline std::ostream &operator<<(std::ostream &out, const kernel_type kernel) {
+    switch (kernel) {
+        case kernel_type::linear:
+            return out << "linear";
+        case kernel_type::polynomial:
+            return out << "polynomial";
+        case kernel_type::rbf:
+            return out << "rbf";
+        default:
+            return out << "unknown";
+    }
+}
 
 }  // namespace plssvm
-
-template <>
-struct fmt::formatter<plssvm::kernel_type> : fmt::formatter<std::string_view> {
-    // parse is inherited from formatter<string_view>.
-    template <typename FormatContext>
-    auto format(plssvm::kernel_type k, FormatContext &ctx) {
-        string_view name = "unknown";
-        switch (k) {
-            case plssvm::kernel_type::linear:
-                name = "linear";
-                break;
-            case plssvm::kernel_type::polynomial:
-                name = "polynomial";
-                break;
-            case plssvm::kernel_type::rbf:
-                name = "rbf";
-                break;
-        }
-        return formatter<string_view>::format(name, ctx);
-    }
-};
