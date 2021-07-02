dab29bdd8ee274176e552439d7f1deed481af8ad
Marcel Breyer <marcel.breyer@ipvs.uni-stuttgart.de>
2021-07-02 12:56:06 +0200

    Add comments to functions and missing [[nodiscard]] and inline specifiers.

--- include/plssvm/string_utility.hpp
+++ include/plssvm/string_utility.hpp
@@ -18,35 +18,76 @@
 #include <string>        // std::char_traits
 #include <string_view>   // std::string_view
 #include <system_error>  // std:errc
-#include <type_traits>   // std::is_floating_point_v, std::is_integral_v, std::is_same_v
+#include <type_traits>   // std::is_floating_point_v, std::is_integral_v, std::is_arithmetic_v
 
 namespace plssvm::util {
 
-bool starts_with(const std::string_view str, const std::string_view start) noexcept {
-    return str.substr(0, start.size()) == start;
+/**
+ * @brief Checks if the string @p str starts with the prefix @p sv.
+ * @param[in] str the string to check
+ * @param[in] sv the string to match against the start of @p str
+ * @return `true` if @p str starts with the string @p sv, otherwise `false` (`[[nodiscard]]`)
+ */
+[[nodiscard]] inline bool starts_with(const std::string_view str, const std::string_view sv) noexcept {
+    return str.substr(0, sv.size()) == sv;
 }
-bool starts_with(const std::string_view str, const char start) noexcept {
-    return !str.empty() && std::char_traits<char>::eq(str.front(), start);
+/**
+ * @brief Checks if the string @p str starts with the character @p c.
+ * @param[in] str the string to check
+ * @param[in] c the character to match against the first character of @p str
+ * @return `true` if @p str starts with the character @p c, otherwise `false` (`[[nodiscard]]`)
+ */
+[[nodiscard]] inline bool starts_with(const std::string_view str, const char c) noexcept {
+    return !str.empty() && std::char_traits<char>::eq(str.front(), c);
 }
-bool ends_with(const std::string_view str, const std::string_view end) noexcept {
-    return str.size() >= end.size() && str.compare(str.size() - end.size(), std::string_view::npos, end) == 0;
+/**
+ * @brief Checks if the string @p str ends with the suffix @p sv.
+ * @param[in] str the string to check
+ * @param[in] sv the string to match against the end of @p str
+ * @return `true` if @p str ends with the string @p sv, otherwise `false` (`[[nodiscard]]`)
+ */
+[[nodiscard]] inline bool ends_with(const std::string_view str, const std::string_view sv) noexcept {
+    return str.size() >= sv.size() && str.compare(str.size() - sv.size(), std::string_view::npos, sv) == 0;
 }
-bool ends_with(const std::string_view str, const char end) noexcept {
-    return !str.empty() && std::char_traits<char>::eq(str.back(), end);
+/**
+ * @brief Checks if the string @p str ends with the character @p c.
+ * @param[in] str the string to check
+ * @param[in] c the character to match against the last character of @p str
+ * @return `true` if @p str ends with the character @p c, otherwise `false` (`[[nodiscard]]`)
+ */
+bool ends_with(const std::string_view str, const char c) noexcept {
+    return !str.empty() && std::char_traits<char>::eq(str.back(), c);
 }
 
-std::string_view trim_left(const std::string_view str) noexcept {
+/**
+ * @brief Returns a new [`std::string_view`](https://en.cppreference.com/w/cpp/string/basic_string_view) equal to @p str where all leding whitespaces are removed.
+ * @param[in] str the string to remove the leading whitespaces
+ * @return the string @p str without leading whitespace (`[[nodiscard]]`)
+ */
+[[nodiscard]] inline std::string_view trim_left(const std::string_view str) noexcept {
     std::string_view::size_type pos = std::min(str.find_first_not_of(' '), str.size());
     return str.substr(pos);
 }
 
+/**
+ * @brief Converts the string @p str to a value of type @p T.
+ * @details If @p T is an integral type [`std::from_chars`](https://en.cppreference.com/w/cpp/utility/from_chars) is used,
+ *          if @p T is a floating point type [`float_fast::from_chars`](https://github.com/fastfloat/fast_float) is used
+ *          and if @p T is neither of both, an exception is thrown.
+ * @tparam T the type to convert the value of @p str to, must be an arithmetic type
+ * @tparam Exception the exception type to throw in case that @p str can't be converted to a value of @p T
+ *         (default: [`std::runtime_error`](https://en.cppreference.com/w/cpp/error/runtime_error)).
+ * @param[in] str the string to convert
+ * @throws Exception if @p str can't be converted to a value of type @p T
+ * @return the value of type @p T denoted by @p str (`[[nodiscard]]`)
+ */
 template <typename T, typename Exception = std::runtime_error>
-T convert_to(std::string_view str) {
+[[nodiscard]] inline T convert_to(std::string_view str) {
     // remove leading whitespaces
     str = trim_left(str);
 
-    // convert string to float or integer
     if constexpr (std::is_floating_point_v<T>) {
+        // convert the string to a floating point value
         T val;
         auto res = fast_float::from_chars(str.data(), str.data() + str.size(), val);
         if (res.ec != std::errc{}) {
@@ -54,6 +95,7 @@ T convert_to(std::string_view str) {
         }
         return val;
     } else if constexpr (std::is_integral_v<T>) {
+        // convert the string to an integral value
         T val;
         auto res = std::from_chars(str.data(), str.data() + str.size(), val);
         if (res.ec != std::errc{}) {
@@ -61,7 +103,8 @@ T convert_to(std::string_view str) {
         }
         return val;
     } else {
-        static_assert(!std::is_same_v<T, T>, "Can only convert arithmetic types!");
+        // can't convert the string to a non-arithmetic type
+        static_assert(std::is_arithmetic_v<T>, "Can only convert arithmetic types!");
     }
 }
 
