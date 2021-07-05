7f6a1a343838511c005a37b4a053894fbd771c9a
Marcel Breyer <marcel.breyer@ipvs.uni-stuttgart.de>
2021-07-05 16:00:11 +0200

    Minor improvements in file class and add documentation.

--- include/plssvm/detail/file.hpp
+++ include/plssvm/detail/file.hpp
@@ -1,9 +1,18 @@
+/**
+ * @file
+ * @author Alexander Van Craen
+ * @author Marcel Breyer
+ * @copyright
+ *
+ * @brief Implements a file class responsible for reading the input file and parsing it into lines.
+ */
+
 #pragma once
 
-#include <plssvm/detail/string_utility.hpp>
-#include <plssvm/exceptions/exceptions.hpp>
+#include <plssvm/detail/string_utility.hpp>  // plssvm::detail::starts_with, plssvm::detail::trim_left
+#include <plssvm/exceptions/exceptions.hpp>  // plssvm::file_not_found_exception
 
-#include <fmt/format.h>
+#include <fmt/core.h>  // fmt::format
 
 // check if memory mapping can be supported
 #if __has_include(<fcntl.h>) && __has_include(<sys/mman.h>) && __has_include(<sys/stat.h>) && __has_include(<unistd.h>)
@@ -15,40 +24,78 @@
     #define PLSSVM_HAS_MEMORY_MAPPING
 #endif
 
-#include <fstream>
-#include <iostream>
-#include <string_view>
-#include <vector>
+#include <cassert>      // assert
+#include <cstddef>      // std::size_t
+#include <fstream>      // std::ifstream
+#include <ios>          // std::ios::beg, std::ios::end, std::streamsize
+#include <iostream>     // std::cerr
+#include <string_view>  // std::string_view
+#include <vector>       // std::vector
 
 namespace plssvm::detail {
 
+/**
+ * @brief The `plssvm::detail::file` class is responsible for reading a file and splitting it into its lines.
+ * @details If the necessary headers are present, the class tries to memory map the given file. If this fails or if the headers are not present,
+ *          the file is read as one blob using [`std::ifstream::read`](https://en.cppreference.com/w/cpp/io/basic_ifstream).
+ */
 class file {
   public:
+    /**
+     * @brief Reads the file denoted by @p filename (possibly using memory mapped IO) and splits it into lines, ignoring empty lines and lines starting with
+     *        the comment token @p comment.
+     * @param[in] filename the file to read and split into lines
+     * @param[in] comment the character used to denote comments
+     */
     file(const std::string_view filename, const char comment) {
 #if defined(PLSSVM_HAS_MEMORY_MAPPING)
+        // headers for memory mapped IO are present -> try it
         this->open_memory_mapped_file(filename);
 #else
+        // memory mapped IO headers are missing -> use std::ifstream instead
         this->open_file(filename);
 #endif
+        // split read data into lines
         this->parse_lines(comment);
     }
+
+    /**
+     * @brief Close the file descriptor used by the memory mapped IO operations or delete the allocated buffer.
+     */
     ~file() {
 #if defined(PLSSVM_HAS_MEMORY_MAPPING)
+        // close file descriptor
         close(file_descriptor_);
-        file_content_ = nullptr;
-#endif
+#else
+        // delete allocated buffer (deleting nullptr is a no-op)
         delete[] file_content_;
+#endif
     }
 
+    /**
+     * @brief Return the number of parsed lines.
+     * @detail All empty lines or lines starting with a comment are ignored.
+     * @return the number of lines after preprocessing (`[[nodiscard]]`)
+     */
     [[nodiscard]] std::size_t num_lines() const noexcept {
         return lines_.size();
     }
+    /**
+     * @brief Return the @p pos line of the parsed file.
+     * @param[in] pos the line to return
+     * @return the line without leading whitespaces
+     */
     [[nodiscard]] std::string_view line(const std::size_t pos) const {
+        assert((pos < this->num_lines) && "Out-of-bounce access!");
         return lines_[pos];
     }
 
   private:
+    /*
+     * Try to read the file using memory mapped IO.
+     */
     void open_memory_mapped_file(const std::string_view filename) {
+        // open the file
         file_descriptor_ = open(filename.data(), O_RDONLY);
         struct stat attr {};
         // check if file could be opened
@@ -57,43 +104,59 @@ class file {
             throw file_not_found_exception{ fmt::format("Couldn't find file: {}!", filename) };
         }
         // memory map file
-        file_content_ = (char *) mmap(nullptr, attr.st_size, PROT_READ, MAP_SHARED, file_descriptor_, 0);
+        file_content_ = static_cast<char *>(mmap(nullptr, attr.st_size, PROT_READ, MAP_SHARED, file_descriptor_, 0));
         // check if memory mapping was successful
-        if ((void *) file_content_ == MAP_FAILED) {
+        if (static_cast<void *>(file_content_) == MAP_FAILED) {
             // memory mapping wasn't successful -> try reading file with std::ifstream
             close(file_descriptor_);
             std::cerr << "Memory mapping failed, falling back to std::ifstream." << std::endl;
             this->open_file(filename);
         }
-        size_ = attr.st_size;
+        // set size
+        num_bytes_ = attr.st_size;
     }
 
+    /*
+     * Read the file using a normal std::ifstream.
+     */
     void open_file(const std::string_view filename) {
+        // open the file
         std::ifstream f{ filename.data() };
         if (f.fail()) {
             throw file_not_found_exception{ fmt::format("Couldn't find file: '{}'!", filename) };
         }
+        // get the size of the file
         f.seekg(0, std::ios::end);
-        size_ = f.tellg();
+        num_bytes_ = f.tellg();
         f.seekg(0);
-        file_content_ = new char[size_];
-        f.read(file_content_, static_cast<std::streamsize>(size_));
+        // allocate the necessary buffer
+        file_content_ = new char[num_bytes_];
+        // read the whole file in one go
+        f.read(file_content_, static_cast<std::streamsize>(num_bytes_));
     }
 
+    /*
+     * Split the file into its lines, ignoring empty lines and lines starting with a comment.
+     */
     void parse_lines(const char comment) {
-        std::string_view file_content_view{ file_content_, size_ };
+        // create view from buffer
+        std::string_view file_content_view{ file_content_, num_bytes_ };
         std::size_t pos = 0;
         while (true) {
+            // find newline
             std::size_t next_pos = file_content_view.find_first_of('\n', pos);
-            if (next_pos == std::string::npos) {
+            if (next_pos == std::string_view::npos) {
                 break;
             }
+            // remove trailing whitespaces
             std::string_view sv = trim_left(std::string_view{ file_content_view.data() + pos, next_pos - pos });
+            // add line iff the line is not empty and doesn't with a comment
             if (!sv.empty() && !starts_with(sv, comment)) {
                 lines_.push_back(sv);
             }
             pos = next_pos + 1;
         }
+        // add last line
         std::string_view sv = trim_left(std::string_view{ file_content_view.data() + pos, file_content_view.size() - pos });
         if (!sv.empty() && !starts_with(sv, comment)) {
             lines_.push_back(sv);
@@ -102,7 +165,7 @@ class file {
 
     int file_descriptor_ = 0;
     char *file_content_ = nullptr;
-    std::size_t size_ = 0;
+    std::size_t num_bytes_ = 0;
     std::vector<std::string_view> lines_{};
 };
 
