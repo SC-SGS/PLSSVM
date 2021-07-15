f560a4e0b39f15d31335bb72b40d3e6b9eaa47ae
Marcel Breyer <marcel.breyer@ipvs.uni-stuttgart.de>
2021-07-15 18:13:58 +0200

    Add documentation.

--- include/plssvm/typedef.hpp
+++ include/plssvm/typedef.hpp
@@ -1,9 +1,29 @@
+/**
+ * @file
+ * @author Alexander Van Craen
+ * @author Marcel Breyer
+ * @copyright
+ *
+ * @brief Global compile-time constants.
+ */
+
 #pragma once
 
 namespace plssvm {
+// TODO: maybe not as global constants?
+// TODO: unsigned or std::size_t?
+// TODO: docu
 
-constexpr unsigned THREADBLOCK_SIZE = 16;   //TODO: immer nur 1 mal
-constexpr unsigned INTERNALBLOCK_SIZE = 6;  // minimal 1
+/// Used for internal caching.
+constexpr unsigned THREADBLOCK_SIZE = 16;
+/// Used for internal caching.
+constexpr unsigned INTERNALBLOCK_SIZE = 6;
+/// Used for internal caching.
 constexpr unsigned THREADS_PER_BLOCK = 1024;
+
+// sanity checks TODO: additional checks?
+static_assert(THREADBLOCK_SIZE > 0, "THREADBLOCK_SIZE must be greater than 0!");
+static_assert(INTERNALBLOCK_SIZE > 0, "INTERNALBLOCK_SIZE must be greater than 0!");
+static_assert(THREADS_PER_BLOCK > 0, "THREADS_PER_BLOCK must be greater than 0!");
 
 }  // namespace plssvm
