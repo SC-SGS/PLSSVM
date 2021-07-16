c9cfdcebd35989d05c353766345218693d08cfc2
Marcel Breyer <marcel.breyer@ipvs.uni-stuttgart.de>
2021-07-16 16:27:24 +0200

    Remove unused functions.

--- include/plssvm/backends/CUDA/cuda-kernel.cuh
+++ include/plssvm/backends/CUDA/cuda-kernel.cuh
@@ -3,24 +3,13 @@
 namespace plssvm {
 
 template <typename real_type>
-__global__ void init(real_type *vec, real_type value, int size);
-
-template <typename real_type>
-__global__ void add_mult(real_type *vec1, const real_type *vec2, const real_type value, const int dim);
-
-template <typename real_type>
-__global__ void add_inplace(real_type *vec1, const real_type *vec2, const int dim);
-
-template <typename real_type>
-__global__ void dot(real_type *a, real_type *b, real_type *c, const int dim);
-
-template <typename real_type>
 __global__ void kernel_q_linear(real_type *q, const real_type *data_d, const real_type *datlast, const int Nrows, const int start, const int end);
 template <typename real_type>
 __global__ void kernel_q_poly(real_type *q, const real_type *data_d, const real_type *datlast, const int Nrows, const int Ncols, const real_type degree, const real_type gamma, const real_type coef0);
 template <typename real_type>
 __global__ void kernel_q_radial(real_type *q, const real_type *data_d, const real_type *datlast, const int Nrows, const int Ncols, const real_type gamma);
 
+// TODO: move separate header
 #if !defined(__CUDA_ARCH__) || __CUDA_ARCH__ >= 600
 #else
 __device__ __forceinline__ double atomicAdd(double *address, double val) {
