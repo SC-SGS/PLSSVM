add9fb5dcc49593e00ca0ba79312e9effe1152d0
Marcel Breyer <marcel.breyer@ipvs.uni-stuttgart.de>
2021-07-16 16:23:56 +0200

    Remove TODOs.

--- src/plssvm/backends/CUDA/svm-kernel.cu
+++ src/plssvm/backends/CUDA/svm-kernel.cu
@@ -56,7 +56,7 @@ __global__ void kernel_linear(const real_type *q, real_type *ret, const real_typ
             #pragma unroll INTERNALBLOCK_SIZE
             for (std::size_t y = 0; y < INTERNALBLOCK_SIZE; ++y) {
                 real_type temp;
-                if (start == 0) {  // TODO: auslagern
+                if (start == 0) {
                     temp = (matr[x][y] + QA_cost - q[i + y] - q[j + x]) * add;
                 } else {
                     temp = matr[x][y] * add;
@@ -68,7 +68,7 @@ __global__ void kernel_linear(const real_type *q, real_type *ret, const real_typ
                     // atomicAdd(&ret[j + x], temp * d[i + y]);
                 } else if (i + x == j + y) {
                     // diagonal
-                    if (start == 0) {  // TODO: auslagern
+                    if (start == 0) {
                         ret_jx += (temp + cost * add) * d[i + y];
                         // atomicAdd(&ret[j + x], (temp + cost * add) * d[i + y]);
                     } else {
@@ -85,7 +85,7 @@ __global__ void kernel_linear(const real_type *q, real_type *ret, const real_typ
 template __global__ void kernel_linear(const float *, float *, const float *, const float *, const float, const float, const int, const int, const int, const int, const int);
 template __global__ void kernel_linear(const double *, double *, const double *, const double *, const double, const double, const int, const int, const int, const int, const int);
 
-template <typename real_type>  // TODO: start / end ?
+template <typename real_type>  // TODO: remove start / end ?
 __global__ void kernel_poly(const real_type *q, real_type *ret, const real_type *d, const real_type *data_d, const real_type QA_cost, const real_type cost, const int Ncols, const int Nrows, const int add, const int start, const int end, const real_type gamma, const real_type coef0, const real_type degree) {
     unsigned int i = blockIdx.x * blockDim.x * INTERNALBLOCK_SIZE;
     unsigned int j = blockIdx.y * blockDim.y * INTERNALBLOCK_SIZE;
