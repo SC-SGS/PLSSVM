9174e63d1434306aef42e20a5c06253425493cfd
Marcel Breyer <marcel.breyer@ipvs.uni-stuttgart.de>
2021-07-16 16:26:59 +0200

    Remove TODOs and remove unused global constant.

--- include/plssvm/typedef.hpp
+++ include/plssvm/typedef.hpp
@@ -10,20 +10,14 @@
 #pragma once
 
 namespace plssvm {
-// TODO: maybe not as global constants?
-// TODO: unsigned or std::size_t?
-// TODO: docu
 
 /// Used for internal caching.
-constexpr unsigned THREADBLOCK_SIZE = 16;
+constexpr unsigned int THREADBLOCK_SIZE = 16;
 /// Used for internal caching.
-constexpr unsigned INTERNALBLOCK_SIZE = 6;
-/// Used for internal caching.
-constexpr unsigned THREADS_PER_BLOCK = 1024;
+constexpr unsigned int INTERNALBLOCK_SIZE = 6;
 
-// sanity checks TODO: additional checks?
+// sanity checks TODO: check CUDA limitations
 static_assert(THREADBLOCK_SIZE > 0, "THREADBLOCK_SIZE must be greater than 0!");
 static_assert(INTERNALBLOCK_SIZE > 0, "INTERNALBLOCK_SIZE must be greater than 0!");
-static_assert(THREADS_PER_BLOCK > 0, "THREADS_PER_BLOCK must be greater than 0!");
 
 }  // namespace plssvm
