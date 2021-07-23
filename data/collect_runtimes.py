6f102e777a2a2e069c6651aafa714cd600221451
Marcel Breyer <marcel.breyer@ipvs.uni-stuttgart.de>
2021-07-23 14:03:17 +0200

    Update parsing rules with new outputs.

--- data/collect_runtimes.py
+++ data/collect_runtimes.py
@@ -15,19 +15,28 @@ import platform
 
 # set up regular expressions
 rx_dict = {
-    'iterations': re.compile(r'Start Iteration:? (?P<iteration>\d+)'),
+    # always present, if print_info is set to true (-q;--quiet is not present on the command line)
+    'backend': re.compile(r'Using (?P<backend>\S+) as backend.'),
+    'num_gpus': re.compile(r'Found (?P<num_gpus>\d+) \S+ device\(s\):'),
+    'num_data_points': re.compile(r'(?P<num_data_points>\d+) data points'),
+    'num_features': re.compile(r'(?P<num_features>\d+) features'),
+    'time_read': re.compile(r'features in (?P<time_read>\d+)ms using'),
+    'file_parser': re.compile(r'(?P<file_parser>\S+) parser'),
+    'time_transform': re.compile(r'Transformed dataset from 2D AoS to 1D SoA in (?P<time_transform>\d+)ms'),
+    'time_setup': re.compile(r'Setup for solving the optimization problem done in (?P<time_setup>\d+)ms.'),
+    'max_iterations': re.compile(r'Iteration \d+ \(max: (?P<max_iterations>\d+)\)'),
+    'iterations': re.compile(r'Finished after (?P<iterations>\d+)'),
+    'time_learn': re.compile(r'using CG in (?P<time_learn>\d+)ms'),
+    'kernel_type': re.compile(r'kernel_type (?P<kernel_type>\S+)'),
     'rho': re.compile(r'rho (?P<rho>[-+]?(\d+(\.\d*)?|\.\d+)([eE][-+]?\d+)?)'),
-    'parser' : re.compile(r'(?P<parser>\S+) parser'),
-    'time_read': re.compile(r'(?P<time>\d+) ms eingelesen'),
-    'time_load': re.compile(r'(?P<time>\d+) ms auf die Gpu geladen'),
-    'time_learn': re.compile(r'((?P<time>\d+) ms gelernt)|(using CG in (?P<time2>\d+)ms)'),
-    'time_write': re.compile(r'((?P<time>\d+) ms geschrieben)|(Wrote model file with \d+ support vectors in (?P<time2>\d+)ms.)'),
-    'points': re.compile(r'(?P<points>\d+) (Datenpunkte)|(data points)'),
-    'features': re.compile(r'(Dimension (?P<dimension>\d+))|((?P<dimension2>\d+) features)'),
-    'num_gpus': re.compile(r'GPUs found: (?P<num_gpus>\d+)'),
-    'transform': re.compile(r'((?P<time>\d+) ms transformiert)|(Transformed dataset \s+ in(?P<time2>\d+)ms)'),
-    'setup': re.compile(r'Setup for solving the optimization problem done in (?P<time>\d+)ms.'),
-    'max_iterations': re.compile(r'Iteration \d+ \(max: (?P<iterations>\d+)\)')
+    'time_write': re.compile(r'Wrote model file with \d+ support vectors in (?P<time_write>\d+)ms.'),
+    # additional information if the plssvm::parameter struct ist outputted
+    'degree': re.compile(r'degree *(?P<degree>[-+]?(\d+(\.\d*)?|\.\d+)([eE][-+]?\d+)?)'),
+    'gamma': re.compile(r'gamma *(?P<gamma>[-+]?(\d+(\.\d*)?|\.\d+)([eE][-+]?\d+)?)'),
+    'coef0': re.compile(r'coef0 *(?P<coef0>[-+]?(\d+(\.\d*)?|\.\d+)([eE][-+]?\d+)?)'),
+    'cost': re.compile(r'cost *(?P<cost>[-+]?(\d+(\.\d*)?|\.\d+)([eE][-+]?\d+)?)'),
+    'epsilon': re.compile(r'epsilon *(?P<epsilon>[-+]?(\d+(\.\d*)?|\.\d+)([eE][-+]?\d+)?)'),
+    'real_type': re.compile(r'real_type *(?P<epsilon>\S+)')
 }
 
 
@@ -65,23 +74,18 @@ def parse_stream(file_stream):
     data = {
         "parse_time": now.strftime("%Y-%m-%d %H:%M:%S"),
         "system": platform.node(),
-            }
+    }
     for line in file_stream:
         # at each line check for a match with a regex
         matches = _parse_line(line)
         for key, match in matches:
             for val in match.groupdict():
                 # check if match exists
-                if match.group(val) != None:
+                if match.group(val) is not None:
                     # check if value already read
                     if key in data:
-                        # update iteration number
-                        if key == 'iterations':
-                            data[key] = max(data[key], match.group(val))
-                        # throw exception if value is already read differently
-                        elif data[key] != match.group(val):
-                            raise Exception(
-                                file_stream, 'illformed in line:', line)
+                        if data[key] != match.group(val):
+                            raise Exception(file_stream, 'ill-formed in line:', line)
                     else:
                         # add new value to dictionary
                         data[key] = match.group(val)
@@ -98,8 +102,8 @@ if __name__ == '__main__':
         stdin = True
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--input", help="the input file", required= not stdin)
-    parser.add_argument("--output", help="the output file to write the samples to (without extension)", required=True)
+    parser.add_argument("--input", help="the input file", required=not stdin)
+    parser.add_argument("--output", help="the output file to write the parsed values to", required=True)
 
     args = parser.parse_args()
 
@@ -117,5 +121,5 @@ if __name__ == '__main__':
     except pd.errors.EmptyDataError:
         pass
 
-    #write results
+    # write results
     data.to_csv(args.output, index=False)
