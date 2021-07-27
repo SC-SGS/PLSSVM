28e6af9e4e6eb39bec234e5a5ec28a03a080faac
Marcel Breyer <marcel.breyer@ipvs.uni-stuttgart.de>
2021-07-27 15:29:53 +0200

    Add sycl backend exception.

---
+++ include/plssvm/backends/SYCL/exceptions.hpp
@@ -0,0 +1,33 @@
+/**
+ * @file
+ * @author Alexander Van Craen
+ * @author Marcel Breyer
+ * @copyright
+ *
+ * @brief Implements custom exception classes specific to the SYCL backend.
+ */
+
+#pragma once
+
+#include "plssvm/exceptions/exceptions.hpp"       // plssvm::exception
+#include "plssvm/exceptions/source_location.hpp"  // plssvm::source_location
+
+#include <string>  // std::string
+
+namespace plssvm::sycl {
+
+/**
+ * @brief Exception type thrown if a problem with the SYCL backend occurs.
+ */
+class backend_exception : public exception {
+  public:
+    /**
+     * @brief Construct a new exception forwarding the exception message and source location to `plssvm::exception`.
+     * @param[in] msg the exception's `what()` message
+     * @param[in] loc the exception's call side information
+     */
+    explicit backend_exception(const std::string &msg, source_location loc = source_location::current()) :
+        ::plssvm::exception{ msg, "sycl::backend_exception", loc } {}
+};
+
+};  // namespace plssvm::sycl
