30f254b1dd0ece1b3bb7c53ccfbef0f6c8ad6f0e
Marcel Breyer <marcel.breyer@ipvs.uni-stuttgart.de>
2021-08-04 16:39:49 +0200

    Print backend and platform summary at end of cmake output.

---
+++ cmake/assemble_summary_string.cmake
@@ -0,0 +1,22 @@
+function(assemble_summary_string out_var)
+    set(PLSSVM_SUMMARY_STRING_ASSEMBLE "")
+    if(DEFINED PLSSVM_CPU_TARGET_ARCHS)
+        # add cpu platform
+        string(APPEND PLSSVM_SUMMARY_STRING_ASSEMBLE " cpu,")
+    endif()
+    if(DEFINED PLSSVM_CUDA_TARGET_ARCHS)
+        # add cuda platform
+        string(APPEND PLSSVM_SUMMARY_STRING_ASSEMBLE " cuda (${PLSSVM_CUDA_TARGET_ARCHS}),")
+    endif()
+    if(DEFINED PLSSVM_AMD_TARGET_ARCHS)
+        # add amd platform
+        string(APPEND PLSSVM_SUMMARY_STRING_ASSEMBLE " amd (${PLSSVM_AMD_TARGET_ARCHS}),")
+    endif()
+    if(DEFINED PLSSVM_INTEL_TARGET_ARCHS)
+        # add intel platform
+        string(APPEND PLSSVM_SUMMARY_STRING_ASSEMBLE " intel,")
+    endif()
+    # remove last comma
+    string(REGEX REPLACE ",$" "" PLSSVM_SUMMARY_STRING_ASSEMBLE "${PLSSVM_SUMMARY_STRING_ASSEMBLE}")
+    set(${out_var} "${PLSSVM_SUMMARY_STRING_ASSEMBLE}" PARENT_SCOPE)
+endfunction()
