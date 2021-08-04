dbd21e98402a94c2d36ccf4ef5b17e1d95d3a290
Marcel Breyer <marcel.breyer@ipvs.uni-stuttgart.de>
2021-08-04 10:53:52 +0200

    Remove unused file.

--- cmake/compile_tests/test_sycl_base.cpp
+++
@@ -1,9 +0,0 @@
-#include "sycl/sycl.hpp"
-
-int main() {
-// TODO: check necessary features?
-#if defined(SYCL_LANGUAGE_VERSION)
-    static_assert(SYCL_LANGUAGE_VERSION >= 202001, "Insufficient SYCL language version!");
-#endif
-    return 0;
-}
