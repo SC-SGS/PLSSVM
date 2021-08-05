0c9c748b6146c764b579face172db6e9e37818b9
Marcel Breyer <marcel.breyer@ipvs.uni-stuttgart.de>
2021-08-05 22:09:58 +0200

    Change to use the error_code wrapper.

--- src/plssvm/backends/OpenCL/detail/device_ptr.cpp
+++ src/plssvm/backends/OpenCL/detail/device_ptr.cpp
@@ -6,8 +6,9 @@
 
 #include "plssvm/backends/OpenCL/detail/device_ptr.hpp"
 
-#include "plssvm/backends/OpenCL/exceptions.hpp"  // plssvm::opencl::backend_exception
-#include "plssvm/detail/assert.hpp"               // PLSSVM_ASSERT
+#include "plssvm/backends/OpenCL/detail/error_code.hpp"  // plssvm::opencl::detail::error_code
+#include "plssvm/backends/OpenCL/exceptions.hpp"         // plssvm::opencl::backend_exception
+#include "plssvm/detail/assert.hpp"                      // PLSSVM_ASSERT
 
 #include "fmt/core.h"  // fmt::format
 
@@ -20,151 +21,19 @@
 
 namespace plssvm::opencl::detail {
 
-inline std::string_view error_code_to_string(const cl_int code) {
-    switch (code) {
-        case 0:
-            return "CL_SUCCESS";
-        case -1:
-            return "CL_DEVICE_NOT_FOUND";
-        case -2:
-            return "CL_DEVICE_NOT_AVAILABLE";
-        case -3:
-            return "CL_COMPILER_NOT_AVAILABLE";
-        case -4:
-            return "CL_MEM_OBJECT_ALLOCATION_FAILURE";
-        case -5:
-            return "CL_OUT_OF_RESOURCES";
-        case -6:
-            return "CL_OUT_OF_HOST_MEMORY";
-        case -7:
-            return "CL_PROFILING_INFO_NOT_AVAILABLE";
-        case -8:
-            return "CL_MEM_COPY_OVERLAP";
-        case -9:
-            return "CL_IMAGE_FORMAT_MISMATCH";
-        case -10:
-            return "CL_IMAGE_FORMAT_NOT_SUPPORTED";
-        case -11:
-            return "CL_BUILD_PROGRAM_FAILURE";
-        case -12:
-            return "CL_MAP_FAILURE";
-        case -13:
-            return "CL_MISALIGNED_SUB_BUFFER_OFFSET";
-        case -14:
-            return "CL_EXEC_STATUS_ERROR_FOR_EVENTS_IN_WAIT_LIST";
-        case -15:
-            return "CL_COMPILE_PROGRAM_FAILURE";
-        case -16:
-            return "CL_LINKER_NOT_AVAILABLE";
-        case -17:
-            return "CL_LINK_PROGRAM_FAILURE";
-        case -18:
-            return "CL_DEVICE_PARTITION_FAILED";
-        case -19:
-            return "CL_KERNEL_ARG_INFO_NOT_AVAILABLE";
-        case -30:
-            return "CL_INVALID_VALUE";
-        case -31:
-            return "CL_INVALID_DEVICE_TYPE";
-        case -32:
-            return "CL_INVALID_PLATFORM";
-        case -33:
-            return "CL_INVALID_DEVICE";
-        case -34:
-            return "CL_INVALID_CONTEXT";
-        case -35:
-            return "CL_INVALID_QUEUE_PROPERTIES";
-        case -36:
-            return "CL_INVALID_COMMAND_QUEUE";
-        case -37:
-            return "CL_INVALID_HOST_PTR";
-        case -38:
-            return "CL_INVALID_MEM_OBJECT";
-        case -39:
-            return "CL_INVALID_IMAGE_FORMAT_DESCRIPTOR";
-        case -40:
-            return "CL_INVALID_IMAGE_SIZE";
-        case -41:
-            return "CL_INVALID_SAMPLER";
-        case -42:
-            return "CL_INVALID_BINARY";
-        case -43:
-            return "CL_INVALID_BUILD_OPTIONS";
-        case -44:
-            return "CL_INVALID_PROGRAM";
-        case -45:
-            return "CL_INVALID_PROGRAM_EXECUTABLE";
-        case -46:
-            return "CL_INVALID_KERNEL_NAME";
-        case -47:
-            return "CL_INVALID_KERNEL_DEFINITION";
-        case -48:
-            return "CL_INVALID_KERNEL";
-        case -49:
-            return "CL_INVALID_ARG_INDEX";
-        case -50:
-            return "CL_INVALID_ARG_VALUE";
-        case -51:
-            return "CL_INVALID_ARG_SIZE";
-        case -52:
-            return "CL_INVALID_KERNEL_ARGS";
-        case -53:
-            return "CL_INVALID_WORK_DIMENSION";
-        case -54:
-            return "CL_INVALID_WORK_GROUP_SIZE";
-        case -55:
-            return "CL_INVALID_WORK_ITEM_SIZE";
-        case -56:
-            return "CL_INVALID_GLOBAL_OFFSET";
-        case -57:
-            return "CL_INVALID_EVENT_WAIT_LIST";
-        case -58:
-            return "CL_INVALID_EVENT";
-        case -59:
-            return "CL_INVALID_OPERATION";
-        case -60:
-            return "CL_INVALID_GL_OBJECT";
-        case -61:
-            return "CL_INVALID_BUFFER_SIZE";
-        case -62:
-            return "CL_INVALID_MIP_LEVEL";
-        case -63:
-            return "CL_INVALID_GLOBAL_WORK_SIZE";
-        case -64:
-            return "CL_INVALID_PROPERTY";
-        case -65:
-            return "CL_INVALID_IMAGE_DESCRIPTOR";
-        case -66:
-            return "CL_INVALID_COMPILER_OPTIONS";
-        case -67:
-            return "CL_INVALID_LINKER_OPTIONS";
-        case -68:
-            return "CL_INVALID_DEVICE_PARTITION_COUNT";
-        case -69:
-            return "CL_INVALID_PIPE_SIZE";
-        case -70:
-            return "CL_INVALID_DEVICE_QUEUE";
-        case -71:
-            return "CL_INVALID_SPEC_ID";
-        case -72:
-            return "CL_MAX_SIZE_RESTRICTION_EXCEEDED";
-        default:
-            return "UNKNOWN";
-    }
-}
-
-inline void device_assert(const cl_int code) {
-    if (code != CL_SUCCESS) {
-        throw backend_exception{ fmt::format("OpenCL assert {}: {}", code, error_code_to_string(code)) };
+inline void device_assert(const error_code ec) {
+    if (!ec) {
+        throw backend_exception{ fmt::format("OpenCL assert ({})", ec) };
     }
 }
 
 template <typename T>
 device_ptr<T>::device_ptr(const size_type size, cl_command_queue queue) :
     queue_{ queue }, size_{ size } {
-    cl_int err;
     cl_context context;
     PLSSVM_OPENCL_ERROR_CHECK(clGetCommandQueueInfo(queue_, CL_QUEUE_CONTEXT, sizeof(cl_context), &context, nullptr));
+
+    error_code err;
     data_ = clCreateBuffer(context, CL_MEM_READ_WRITE, size_ * sizeof(value_type), nullptr, &err);
     PLSSVM_OPENCL_ERROR_CHECK(err);
 }
@@ -210,7 +79,7 @@ void device_ptr<T>::memset(const value_type value, const size_type pos, const si
         throw backend_exception{ fmt::format("Illegal access in memset!: {} >= {}", pos, size_) };
     }
     const size_type rcount = std::min(count, size_ - pos);
-    cl_int err;
+    error_code err;
     err = clEnqueueFillBuffer(queue_, data_, &value, sizeof(value_type), pos * sizeof(value_type), rcount * sizeof(value_type), 0, nullptr, nullptr);
     PLSSVM_OPENCL_ERROR_CHECK(err);
     PLSSVM_OPENCL_ERROR_CHECK(clFinish(queue_));
@@ -243,7 +112,7 @@ void device_ptr<T>::memcpy_to_device(const_pointer data_to_copy, const size_type
     PLSSVM_ASSERT(data_ != nullptr, "Invalid data pointer!");
 
     const size_type rcount = std::min(count, size_ - pos);
-    cl_int err;
+    error_code err;
     err = clEnqueueWriteBuffer(queue_, data_, CL_TRUE, pos * sizeof(value_type), rcount * sizeof(value_type), data_to_copy, 0, nullptr, nullptr);
     PLSSVM_OPENCL_ERROR_CHECK(err);
     PLSSVM_OPENCL_ERROR_CHECK(clFinish(queue_));
@@ -276,7 +145,7 @@ void device_ptr<T>::memcpy_to_host(pointer buffer, const size_type pos, const si
     PLSSVM_ASSERT(data_ != nullptr, "Invalid data pointer!");
 
     const size_type rcount = std::min(count, size_ - pos);
-    cl_int err;
+    error_code err;
     err = clEnqueueReadBuffer(queue_, data_, CL_TRUE, pos * sizeof(value_type), rcount * sizeof(value_type), buffer, 0, nullptr, nullptr);
     PLSSVM_OPENCL_ERROR_CHECK(err);
     PLSSVM_OPENCL_ERROR_CHECK(clFinish(queue_));
