ef15b31a5d58245c9d3af231c97025ba07db3afd
vancraar <vancraar@users.noreply.github.com>
2022-01-20 14:35:34 +0000

    deploy: 9548b19a37b34cadc1883541f0ebce17157d57ae

--- search/all_9.js
+++ search/all_9.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['learn_119',['learn',['../classplssvm_1_1csvm.html#af54a2f0f53af7c0fa6b1be469462d3ca',1,'plssvm::csvm']]],
-  ['least_2dsquares_20support_2dvector_20machine_120',['Least-Squares Support-Vector Machine',['../index.html',1,'']]],
+  ['least_20squares_20support_20vector_20machine_120',['Least Squares Support Vector Machine',['../index.html',1,'']]],
   ['line_121',['line',['../classplssvm_1_1detail_1_1file__reader.html#ab8a3cf538ae798db7feaa80339dd505d',1,'plssvm::detail::file_reader::line()'],['../classplssvm_1_1source__location.html#a28c3a2b57d878cee5619037483b24031',1,'plssvm::source_location::line()']]],
   ['linear_122',['linear',['../namespaceplssvm.html#a13d04ccec8f2cd5794a5d2d73dd99703a9a932b3cb396238423eb2f33ec17d6aa',1,'plssvm']]],
   ['lines_123',['lines',['../classplssvm_1_1detail_1_1file__reader.html#a258d5cb35ba27e6cdc372f49d7a6a1ca',1,'plssvm::detail::file_reader']]],
--- search/pages_0.js
+++ search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['least_2dsquares_20support_2dvector_20machine_568',['Least-Squares Support-Vector Machine',['../index.html',1,'']]]
+  ['least_20squares_20support_20vector_20machine_568',['Least Squares Support Vector Machine',['../index.html',1,'']]]
 ];
