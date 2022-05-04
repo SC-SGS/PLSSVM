eb6f104ef5f7691372e893b43ce629ea0afad464
Marcel Breyer <marcel.breyer@ipvs.uni-stuttgart.de>
2022-05-04 09:08:21 +0200

    Remove erroneous };

--- src/plssvm/target_platforms.cpp
+++ src/plssvm/target_platforms.cpp
@@ -31,7 +31,6 @@ std::vector<target_platform> list_available_target_platforms() {
 #endif
 #if defined(PLSSVM_HAS_INTEL_TARGET)
     available_targets.push_back(target_platform::gpu_intel);
-};
 #endif
 return available_targets;
 }
