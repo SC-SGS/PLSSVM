4cca98aa1c79b7848142682ecde4b7970685dd34
Marcel Breyer <marcel.breyer@ipvs.uni-stuttgart.de>
2022-05-10 14:05:20 +0200

    Fix @params annotation in include/plssvm/backends/HIP/predict_kernel.hip.hpp
    Co-authored-by: Alexander Van Craen <40516079+vancraar@users.noreply.github.com>

--- include/plssvm/backends/HIP/predict_kernel.hip.hpp
+++ include/plssvm/backends/HIP/predict_kernel.hip.hpp
@@ -46,7 +46,7 @@ __global__ void device_kernel_w_linear(real_type *w_d, const real_type *data_d, 
  * @brief Predicts the labels for data points using the polynomial kernel function.
  * @details Currently only single GPU execution is supported.
  * @tparam real_type the type of the data
- * @param[in] out_d the calculated predictions
+ * @param[out] out_d the calculated predictions
  * @param[in] data_d the one-dimension support vector matrix
  * @param[in] data_last_d the last row of the support vector matrix
  * @param[in] alpha_d the previously calculated weight for each data point
