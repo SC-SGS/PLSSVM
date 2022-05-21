95a1ca237789e77e7b2eb13fd36432f8800f137c
Marcel Breyer <breyerml@studi.informatik.uni-stuttgart.de>
2022-05-21 14:44:27 +0200

    Add missing static_asserts.

--- include/plssvm/model.hpp
+++ include/plssvm/model.hpp
@@ -33,6 +33,9 @@ namespace plssvm {
 template <typename T, typename U = int>
 class model {
     static_assert(std::is_same_v<T, float> || std::is_same_v<T, double>, "The first template type can only be 'float' or 'double'!");
+    static_assert(std::is_arithmetic_v<U> || std::is_same_v<U, std::string>, "The second template type can only be an arithmetic type or 'std::string'!");
+    // because std::vector<bool> is evil
+    static_assert(!std::is_same_v<U, bool>, "The second template type must NOT be 'bool'!");
 
   public:
     using real_type = T;
