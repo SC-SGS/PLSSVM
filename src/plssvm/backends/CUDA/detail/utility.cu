360c3b0c502426f416794893db24da1c29793d23
Marcel Breyer <marcel.breyer@ipvs.uni-stuttgart.de>
2022-11-14 14:58:30 +0100

    Add some tests for the CUDA utility functions.

--- src/plssvm/backends/CUDA/detail/utility.cu
+++ src/plssvm/backends/CUDA/detail/utility.cu
@@ -16,7 +16,7 @@ namespace plssvm::cuda::detail {
 
 void gpu_assert(const cudaError_t code) {
     if (code != cudaSuccess) {
-        throw backend_exception{ fmt::format("CUDA assert {}: {}", cudaGetErrorName(code), cudaGetErrorString(code)) };
+        throw backend_exception{ fmt::format("CUDA assert '{}' ({}): {}", cudaGetErrorName(code), code, cudaGetErrorString(code)) };
     }
 }
 
@@ -28,7 +28,7 @@ void gpu_assert(const cudaError_t code) {
 
 void set_device(const int device) {
     if (device < 0 || device >= static_cast<int>(get_device_count())) {
-        throw backend_exception{ fmt::format("Illegal device ID! Must be in range: [0, {}) but is {}.", get_device_count(), device) };
+        throw backend_exception{ fmt::format("Illegal device ID! Must be in range: [0, {}) but is {}!", get_device_count(), device) };
     }
     PLSSVM_CUDA_ERROR_CHECK(cudaSetDevice(device));
 }
@@ -39,7 +39,7 @@ void peek_at_last_error() {
 
 void device_synchronize(const int device) {
     if (device < 0 || device >= static_cast<int>(get_device_count())) {
-        throw backend_exception{ fmt::format("Illegal device ID! Must be in range: [0, {}) but is {}.", get_device_count(), device) };
+        throw backend_exception{ fmt::format("Illegal device ID! Must be in range: [0, {}) but is {}!", get_device_count(), device) };
     }
     peek_at_last_error();
     PLSSVM_CUDA_ERROR_CHECK(cudaSetDevice(device));
---
+++ tests/backends/CUDA/utility.cpp
@@ -0,0 +1,55 @@
+/**
+ * @author Alexander Van Craen
+ * @author Marcel Breyer
+ * @copyright 2018-today The PLSSVM project - All Rights Reserved
+ * @license This file is part of the PLSSVM project which is released under the MIT license.
+ *          See the LICENSE.md file in the project root for full license information.
+ *
+ * @brief Tests for the custom utility functions related to the CUDA backend.
+ */
+
+// necessary only for compiling the tests
+#include "cuda_runtime.h"  // cudaError_t
+
+#include "plssvm/backends/CUDA/detail/utility.cuh"  // plssvm::cuda::{set_device, device_synchronize}
+
+#include "plssvm/backends/CUDA/exceptions.hpp"  // plssvm::cuda::backend_exception
+
+#include "../../custom_test_macros.hpp"  // EXPECT_THROW_WHAT, EXPECT_THROW_WHAT_MATCHER
+
+#include "fmt/core.h"              // fmt::format
+#include "gmock/gmock-matchers.h"  // ::testing::StartsWith
+#include "gtest/gtest.h"           // TEST, EXPECT_GE, EXPECT_NO_THROW, ::testing::StartsWith
+
+TEST(CUDAUtility, gpu_assert) {
+    // cudaSuccess must not throw
+    EXPECT_NO_THROW(PLSSVM_CUDA_ERROR_CHECK(cudaSuccess));
+    EXPECT_NO_THROW(plssvm::cuda::detail::gpu_assert(cudaSuccess));
+
+    // any other code must throw
+    EXPECT_THROW_WHAT_MATCHER(PLSSVM_CUDA_ERROR_CHECK(cudaErrorInvalidValue),
+                              plssvm::cuda::backend_exception,
+                              ::testing::StartsWith("CUDA assert 'cudaErrorInvalidValue' (1):"));
+    EXPECT_THROW_WHAT_MATCHER(plssvm::cuda::detail::gpu_assert(cudaErrorInvalidValue),
+                              plssvm::cuda::backend_exception,
+                              ::testing::StartsWith("CUDA assert 'cudaErrorInvalidValue' (1):"));
+}
+
+TEST(CUDAUtility, get_device_count) {
+    // must not return a negative number
+    EXPECT_GE(plssvm::cuda::detail::get_device_count(), 0);
+}
+
+TEST(CUDAUtility, set_device) {
+    // exception must be thrown if an illegal device ID has been provided
+    EXPECT_THROW_WHAT(plssvm::cuda::detail::set_device(plssvm::cuda::detail::get_device_count()),
+                      plssvm::cuda::backend_exception,
+                      fmt::format("Illegal device ID! Must be in range: [0, {}) but is {}!", plssvm::cuda::detail::get_device_count(), plssvm::cuda::detail::get_device_count()));
+}
+
+TEST(CUDAUtility, device_synchronize) {
+    // exception must be thrown if an illegal device ID has been provided
+    EXPECT_THROW_WHAT(plssvm::cuda::detail::device_synchronize(plssvm::cuda::detail::get_device_count()),
+                      plssvm::cuda::backend_exception,
+                      fmt::format("Illegal device ID! Must be in range: [0, {}) but is {}!", plssvm::cuda::detail::get_device_count(), plssvm::cuda::detail::get_device_count()));
+}
