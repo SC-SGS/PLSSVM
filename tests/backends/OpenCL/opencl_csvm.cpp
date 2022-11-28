a8f8dd3cebbf4e4a0158205cb72a2c54ddc51dad
Marcel Breyer <marcel.breyer@ipvs.uni-stuttgart.de>
2022-11-28 17:40:18 +0100

    Add constructor tests.

--- tests/backends/OpenCL/opencl_csvm.cpp
+++ tests/backends/OpenCL/opencl_csvm.cpp
@@ -11,13 +11,89 @@
 #include "backends/generic_csvm_tests.hpp"  // generic::write_model_test, generic::generate_q_test, generic::device_kernel_test, generic::predict_test, generic::accuracy_test
 #include "utility.hpp"                      // util::google_test::parameter_definition, util::google_test::parameter_definition_to_name
 
-#include "plssvm/backends/OpenCL/csvm.hpp"   // plssvm::opencl::csvm
-#include "plssvm/kernel_function_types.hpp"  // plssvm::kernel_type
-#include "plssvm/parameter.hpp"              // plssvm::parameter
+#include "plssvm/backends/OpenCL/csvm.hpp"        // plssvm::opencl::csvm
+#include "plssvm/backends/OpenCL/exceptions.hpp"  // plssvm::opencl::backend_exception
+#include "plssvm/kernel_function_types.hpp"       // plssvm::kernel_type
+#include "plssvm/parameter.hpp"                   // plssvm::parameter
 
 #include "gtest/gtest.h"  // ::testing::StaticAssertTypeEq, ::testing::Test, ::testing::Types, TYPED_TEST_SUITE, TYPED_TEST
 
-// TODO: Konstruktors!
+class OpenCLCSVM : public ::testing::Test, private util::redirect_output {};
+
+// check whether the constructor correctly fails when using an incompatible target platform
+TEST_F(OpenCLCSVM, construct_parameter) {
+    // the automatic target platform must always be available
+    EXPECT_NO_THROW(plssvm::opencl::csvm{ plssvm::parameter{} });
+}
+TEST_F(OpenCLCSVM, construct_target_and_parameter) {
+    // create parameter struct
+    const plssvm::parameter params{};
+
+    // every target is allowed for OpenCL
+#if defined(PLSSVM_HAS_CPU_TARGET)
+    EXPECT_NO_THROW((plssvm::opencl::csvm{ plssvm::target_platform::cpu, params }));
+#else
+    EXPECT_THROW_WHAT((plssvm::opencl::csvm{ plssvm::target_platform::cpu, params }),
+                      plssvm::opencl::backend_exception,
+                      "Requested target platform 'cpu' that hasn't been enabled using PLSSVM_TARGET_PLATFORMS!");
+#endif
+#if defined(PLSSVM_HAS_NVIDIA_TARGET)
+    EXPECT_NO_THROW((plssvm::opencl::csvm{ plssvm::target_platform::gpu_nvidia, params }));
+#else
+    EXPECT_THROW_WHAT((plssvm::opencl::csvm{ plssvm::target_platform::gpu_nvidia, params }),
+                      plssvm::opencl::backend_exception,
+                      "Requested target platform 'gpu_nvidia' that hasn't been enabled using PLSSVM_TARGET_PLATFORMS!");
+#endif
+#if defined(PLSSVM_HAS_AMD_TARGET)
+    EXPECT_NO_THROW((plssvm::opencl::csvm{ plssvm::target_platform::gpu_amd, params }));
+#else
+    EXPECT_THROW_WHAT((plssvm::opencl::csvm{ plssvm::target_platform::gpu_amd, params }),
+                      plssvm::opencl::backend_exception,
+                      "Requested target platform 'gpu_amd' that hasn't been enabled using PLSSVM_TARGET_PLATFORMS!");
+#endif
+#if defined(PLSSVM_HAS_INTEL_TARGET)
+    EXPECT_NO_THROW((plssvm::opencl::csvm{ plssvm::target_platform::gpu_intel, params }));
+#else
+    EXPECT_THROW_WHAT((plssvm::opencl::csvm{ plssvm::target_platform::gpu_intel, params }),
+                      plssvm::opencl::backend_exception,
+                      "Requested target platform 'gpu_intel' that hasn't been enabled using PLSSVM_TARGET_PLATFORMS!");
+#endif
+}
+TEST_F(OpenCLCSVM, construct_target_and_named_args) {
+    // every target is allowed for OpenCL
+#if defined(PLSSVM_HAS_CPU_TARGET)
+    EXPECT_NO_THROW((plssvm::opencl::csvm{ plssvm::target_platform::cpu, plssvm::kernel_type = plssvm::kernel_function_type::linear, plssvm::cost = 2.0 }));
+    EXPECT_NO_THROW((plssvm::opencl::csvm{ plssvm::target_platform::cpu, plssvm::cost = 2.0 }));
+#else
+    EXPECT_THROW_WHAT((plssvm::opencl::csvm{ plssvm::target_platform::cpu, plssvm::kernel_type = plssvm::kernel_function_type::linear, plssvm::cost = 2.0 }),
+                      plssvm::opencl::backend_exception,
+                      "Requested target platform 'cpu' that hasn't been enabled using PLSSVM_TARGET_PLATFORMS!");
+#endif
+#if defined(PLSSVM_HAS_NVIDIA_TARGET)
+    EXPECT_NO_THROW((plssvm::opencl::csvm{ plssvm::target_platform::gpu_nvidia, plssvm::kernel_type = plssvm::kernel_function_type::linear, plssvm::cost = 2.0 }));
+    EXPECT_NO_THROW((plssvm::opencl::csvm{ plssvm::target_platform::gpu_nvidia, plssvm::cost = 2.0 }));
+#else
+    EXPECT_THROW_WHAT((plssvm::opencl::csvm{ plssvm::target_platform::gpu_nvidia, plssvm::kernel_type = plssvm::kernel_function_type::linear, plssvm::cost = 2.0 }),
+                      plssvm::opencl::backend_exception,
+                      "Requested target platform 'gpu_nvidia' that hasn't been enabled using PLSSVM_TARGET_PLATFORMS!");
+#endif
+#if defined(PLSSVM_HAS_AMD_TARGET)
+    EXPECT_NO_THROW((plssvm::opencl::csvm{ plssvm::target_platform::gpu_amd, plssvm::kernel_type = plssvm::kernel_function_type::linear, plssvm::cost = 2.0 }));
+    EXPECT_NO_THROW((plssvm::opencl::csvm{ plssvm::target_platform::gpu_amd, plssvm::kernel_type = plssvm::cost = 2.0 }));
+#else
+    EXPECT_THROW_WHAT((plssvm::opencl::csvm{ plssvm::target_platform::gpu_amd, plssvm::kernel_type = plssvm::kernel_function_type::linear, plssvm::cost = 2.0 }),
+                      plssvm::opencl::backend_exception,
+                      "Requested target platform 'gpu_amd' that hasn't been enabled using PLSSVM_TARGET_PLATFORMS!");
+#endif
+#if defined(PLSSVM_HAS_INTEL_TARGET)
+    EXPECT_NO_THROW((plssvm::opencl::csvm{ plssvm::target_platform::gpu_intel, plssvm::kernel_type = plssvm::kernel_function_type::linear, plssvm::cost = 2.0 }));
+    EXPECT_NO_THROW((plssvm::opencl::csvm{ plssvm::target_platform::gpu_intel, plssvm::cost = 2.0 }));
+#else
+    EXPECT_THROW_WHAT((plssvm::opencl::csvm{ plssvm::target_platform::gpu_intel, plssvm::kernel_type = plssvm::kernel_function_type::linear, plssvm::cost = 2.0 }),
+                      plssvm::opencl::backend_exception,
+                      "Requested target platform 'gpu_intel' that hasn't been enabled using PLSSVM_TARGET_PLATFORMS!");
+#endif
+}
 
 template <typename T, plssvm::kernel_function_type kernel>
 struct csvm_test_type {
