187ede07a854278d304d757ca644148173446c18
Marcel Breyer <marcel.breyer@ipvs.uni-stuttgart.de>
2022-12-21 11:40:01 +0100

    Fix include.

--- include/plssvm/parameter.hpp
+++ include/plssvm/parameter.hpp
@@ -14,8 +14,7 @@
 #pragma once
 
 #include "plssvm/default_value.hpp"          // plssvm::default_value, plssvm::is_default_value_v
-#include "plssvm/detail/type_traits.hpp"     // plssvm::detail::{remove_cvref_t, always_false_v}
-#include "plssvm/detail/type_traits.hpp"     // PLSSVM_REQUIRES
+#include "plssvm/detail/type_traits.hpp"     // PLSSVM_REQUIRES, plssvm::detail::{remove_cvref_t, always_false_v}
 #include "plssvm/detail/utility.hpp"         // plssvm::detail::unreachable
 #include "plssvm/kernel_function_types.hpp"  // plssvm::kernel_function_type, plssvm::kernel_function_type_to_math_string
 
