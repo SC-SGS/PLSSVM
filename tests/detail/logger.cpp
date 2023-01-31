c556f1af0b23bd1d8daa23741f6a33426862f0c2
Marcel Breyer <marcel.breyer@ipvs.uni-stuttgart.de>
2023-01-31 16:45:29 +0100

    Add dummy test files.

---
+++ tests/detail/logger.cpp
@@ -0,0 +1,15 @@
+/**
+ * @author Alexander Van Craen
+ * @author Marcel Breyer
+ * @copyright 2018-today The PLSSVM project - All Rights Reserved
+ * @license This file is part of the PLSSVM project which is released under the MIT license.
+ *          See the LICENSE.md file in the project root for full license information.
+ *
+ * @brief Tests for the logging function.
+ */
+
+#include "plssvm/detail/logger.hpp"
+
+#include "gtest/gtest.h"  // TEST, TYPED_TEST_SUITE, TYPED_TEST, EXPECT_EQ, EXPECT_TRUE, EXPECT_DEATH, ::testing::{Test, Types}
+
+// TODO: implement tests
