b2f3c3b02821c9bf0f1880a6bb6c4f9f34dba12a
Marcel Breyer <marcel.breyer@ipvs.uni-stuttgart.de>
2023-02-10 11:21:29 +0100

    Add tests for the logging function.

--- tests/detail/logger.cpp
+++ tests/detail/logger.cpp
@@ -10,6 +10,52 @@
 
 #include "plssvm/detail/logger.hpp"
 
-#include "gtest/gtest.h"  // TEST, TYPED_TEST_SUITE, TYPED_TEST, EXPECT_EQ, EXPECT_TRUE, EXPECT_DEATH, ::testing::{Test, Types}
+#include "plssvm/constants.hpp"  // plssvm::verbose
 
-// TODO: implement tests
+#include "../utility.hpp"  // util::redirect_output
+
+#include "gtest/gtest.h"  // TEST_F, EXPECT_EQ, EXPECT_TRUE, ::testing::Test
+
+class Logger : public ::testing::Test, public util::redirect_output<> {};
+
+TEST_F(Logger, enabled_logging) {
+    // explicitly enable logging
+    plssvm::verbose = true;
+
+    // log a message
+    plssvm::detail::log("Hello, World!");
+
+    // check captured output
+    EXPECT_EQ(this->get_capture(), "Hello, World!");
+}
+TEST_F(Logger, enabled_logging_with_args) {
+    // explicitly enable logging
+    plssvm::verbose = true;
+
+    // log a message
+    plssvm::detail::log("int: {}, float: {}, str: {}", 42, 1.5, "abc");
+
+    // check captured output
+    EXPECT_EQ(this->get_capture(), "int: 42, float: 1.5, str: abc");
+}
+
+TEST_F(Logger, disabled_logging) {
+    // explicitly disable logging
+    plssvm::verbose = false;
+
+    // log message
+    plssvm::detail::log("Hello, World!");
+
+    // since logging has been disabled, nothing should have been captured
+    EXPECT_TRUE(this->get_capture().empty());
+}
+TEST_F(Logger, disabled_logging_with_args) {
+    // explicitly disable logging
+    plssvm::verbose = false;
+
+    // log message
+    plssvm::detail::log("int: {}, float: {}, str: {}", 42, 1.5, "abc");
+
+    // since logging has been disabled, nothing should have been captured
+    EXPECT_TRUE(this->get_capture().empty());
+}
