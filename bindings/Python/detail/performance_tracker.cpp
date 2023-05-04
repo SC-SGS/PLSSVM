445913399658df06035e85a264e71d1a21dd9334
Marcel Breyer <marcel.breyer@ipvs.uni-stuttgart.de>
2023-05-04 17:51:32 +0200

    Add Python binding to generically track a string entry.

--- bindings/Python/detail/performance_tracker.cpp
+++ bindings/Python/detail/performance_tracker.cpp
@@ -20,12 +20,15 @@ void init_performance_tracker(py::module_ &m) {
 
     // bind the performance tracker
     py::class_<plssvm::detail::performance_tracker>(detail_module, "PerformanceTracker")
-        .def_static("add_parameter_tracking_entry", [](const plssvm::parameter &params) { plssvm::detail::performance_tracker::add_tracking_entry(
-                                                                                              plssvm::detail::tracking_entry{ "parameter", "", params }); })
+        .def_static("add_string_tracking_entry", [](const std::string &category, const std::string & name, const std::string &value) {
+            plssvm::detail::performance_tracker::add_tracking_entry(plssvm::detail::tracking_entry{ category, name, value });
+        })
+        .def_static("add_parameter_tracking_entry", [](const plssvm::parameter &params) {
+            plssvm::detail::performance_tracker::add_tracking_entry(plssvm::detail::tracking_entry{ "parameter", "", params });
+        })
         .def_static("pause", &plssvm::detail::performance_tracker::pause_tracking, "pause performance tracking")
         .def_static("resume", &plssvm::detail::performance_tracker::resume_tracking, "resume performance tracking")
-        .def_static(
-            "save_to", py::overload_cast<const std::string &>(&plssvm::detail::performance_tracker::save), "save the performance tracking results to the specified yaml file");
+        .def_static("save_to", py::overload_cast<const std::string &>(&plssvm::detail::performance_tracker::save), "save the performance tracking results to the specified yaml file");
 
 #endif
 }
