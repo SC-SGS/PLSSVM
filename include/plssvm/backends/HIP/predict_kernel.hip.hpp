7a651ad853d045f17031855f1c3bc7e0c09526fd
Marcel Breyer <marcel.breyer@ipvs.uni-stuttgart.de>
2023-07-07 15:52:11 +0200

    Update function parameter type and add documentation.

--- include/plssvm/backends/HIP/predict_kernel.hip.hpp
+++ include/plssvm/backends/HIP/predict_kernel.hip.hpp
@@ -13,49 +13,82 @@
 #define PLSSVM_BACKENDS_HIP_PREDICT_KERNEL_HIP_HPP_
 #pragma once
 
-#include "plssvm/constants.hpp"  // plssvm::real_type, plssvm::kernel_index_type
+#include "plssvm/constants.hpp"  // plssvm::real_type
 
 #include "hip/hip_runtime.h"
 #include "hip/hip_runtime_api.h"
 
-
 namespace plssvm::hip {
 
-__global__ void device_kernel_w_linear(real_type *w_d, const real_type *alpha_d, const real_type *sv_d, const kernel_index_type num_classes, const kernel_index_type num_sv, const kernel_index_type num_features) {
-    const kernel_index_type feature_idx = blockIdx.x * blockDim.x + threadIdx.x;
-    const kernel_index_type class_idx = blockIdx.y * blockDim.y + threadIdx.y;
+/**
+ * @brief Calculate the `q` vector used to speedup the prediction using the linear kernel function.
+ * @param[in,out] w_d the vector to speedup the linear prediction
+ * @param[in] alpha_d the previously learned weights
+ * @param[in] sv_d the support vectors
+ * @param[in] num_classes the number of classes
+ * @param[in] num_sv the number of support vectors
+ * @param[in] num_features the number of features per support vector
+ */
+__global__ void device_kernel_w_linear(real_type *w_d, const real_type *alpha_d, const real_type *sv_d, const unsigned long long num_classes, const unsigned long long num_sv, const unsigned long long num_features) {
+    const unsigned long long feature_idx = blockIdx.x * blockDim.x + threadIdx.x;
+    const unsigned long long class_idx = blockIdx.y * blockDim.y + threadIdx.y;
 
     if (feature_idx < num_features && class_idx < num_classes) {
         real_type temp{ 0.0 };
-        for (kernel_index_type sv = 0; sv < num_sv; ++sv) {
+        for (unsigned long long sv = 0; sv < num_sv; ++sv) {
             temp += alpha_d[class_idx * num_sv + sv] * sv_d[sv * num_features + feature_idx];
         }
         w_d[class_idx * num_features + feature_idx] = temp;
     }
 }
 
-__global__ void device_kernel_predict_linear(real_type *out_d, const real_type *w_d, const real_type *rho_d, const real_type *predict_points_d, const kernel_index_type num_classes, const kernel_index_type num_predict_points, const kernel_index_type num_features) {
-    const kernel_index_type predict_point_idx = blockIdx.x * blockDim.x + threadIdx.x;
-    const kernel_index_type class_idx = blockIdx.y * blockDim.y + threadIdx.y;
+/**
+ * @brief Predict the @p predict_points_d using the linear kernel speeding up the calculation using the @p w_d vector.
+ * @param[out] out_d the predicted values
+ * @param[in] w_d the vector to speedup the calculations
+ * @param[in] rho_d the previously learned bias
+ * @param[in] predict_points_d the data points to predict
+ * @param[in] num_classes the number of classes
+ * @param[in] num_predict_points the number of data points to predict
+ * @param[in] num_features the number of features per data point
+ */
+__global__ void device_kernel_predict_linear(real_type *out_d, const real_type *w_d, const real_type *rho_d, const real_type *predict_points_d, const unsigned long long num_classes, const unsigned long long num_predict_points, const unsigned long long num_features) {
+    const unsigned long long predict_point_idx = blockIdx.x * blockDim.x + threadIdx.x;
+    const unsigned long long class_idx = blockIdx.y * blockDim.y + threadIdx.y;
 
     if (predict_point_idx < num_predict_points && class_idx < num_classes) {
         real_type temp{ 0.0 };
-        for (kernel_index_type dim = 0; dim < num_features; ++dim) {
+        for (unsigned long long dim = 0; dim < num_features; ++dim) {
             temp += w_d[class_idx * num_features + dim] * predict_points_d[predict_point_idx * num_features + dim];
         }
         out_d[predict_point_idx * num_classes + class_idx] = temp - rho_d[class_idx];
     }
 }
 
-__global__ void device_kernel_predict_polynomial(real_type *out_d, const real_type *alpha_d, const real_type *rho_d, const real_type *sv_d, const real_type *predict_points_d, const kernel_index_type num_classes, const kernel_index_type num_sv, const kernel_index_type num_predict_points, const kernel_index_type num_features, const int degree, const real_type gamma, const real_type coef0) {
-    const kernel_index_type sv_idx = blockIdx.x * blockDim.x + threadIdx.x;
-    const kernel_index_type predict_points_idx = blockIdx.y * blockDim.y + threadIdx.y;
-    const kernel_index_type class_idx = blockIdx.z * blockDim.z + threadIdx.z;
+/**
+ * @brief Predict the @p predict_points_d using the polynomial.
+ * @param[in] out_d the predicted values
+ * @param[in] alpha_d the previously learned weights
+ * @param[in] rho_d the previously learned biases
+ * @param[in] sv_d the support vectors
+ * @param[in] predict_points_d the data points to predict
+ * @param[in] num_classes the number of classes
+ * @param[in] num_sv the number of support vectors
+ * @param[in] num_predict_points the number of data points to predict
+ * @param[in] num_features the number of features per data point
+ * @param[in] degree the parameter in the polynomial kernel function
+ * @param[in] gamma the parameter in the polynomial kernel function
+ * @param[in] coef0 the parameter in the polynomial kernel function
+ */
+__global__ void device_kernel_predict_polynomial(real_type *out_d, const real_type *alpha_d, const real_type *rho_d, const real_type *sv_d, const real_type *predict_points_d, const unsigned long long num_classes, const unsigned long long num_sv, const unsigned long long num_predict_points, const unsigned long long num_features, const int degree, const real_type gamma, const real_type coef0) {
+    const unsigned long long sv_idx = blockIdx.x * blockDim.x + threadIdx.x;
+    const unsigned long long predict_points_idx = blockIdx.y * blockDim.y + threadIdx.y;
+    const unsigned long long class_idx = blockIdx.z * blockDim.z + threadIdx.z;
 
     if (sv_idx < num_sv && predict_points_idx < num_predict_points && class_idx < num_classes) {
         real_type temp{ 0.0 };
         // perform dot product
-        for (kernel_index_type dim = 0; dim < num_features; ++dim) {
+        for (unsigned long long dim = 0; dim < num_features; ++dim) {
             temp += sv_d[sv_idx * num_features + dim] * predict_points_d[predict_points_idx * num_features + dim];
         }
 
@@ -69,15 +102,28 @@ __global__ void device_kernel_predict_polynomial(real_type *out_d, const real_ty
     }
 }
 
-__global__ void device_kernel_predict_rbf(real_type *out_d, const real_type *alpha_d, const real_type *rho_d, const real_type *sv_d, const real_type *predict_points_d, const kernel_index_type num_classes, const kernel_index_type num_sv, const kernel_index_type num_predict_points, const kernel_index_type num_features, const real_type gamma) {
-    const kernel_index_type sv_idx = blockIdx.x * blockDim.x + threadIdx.x;
-    const kernel_index_type predict_points_idx = blockIdx.y * blockDim.y + threadIdx.y;
-    const kernel_index_type class_idx = blockIdx.z * blockDim.z + threadIdx.z;
+/**
+ * @brief Predict the @p predict_points_d using the rbf.
+ * @param[in] out_d the predicted values
+ * @param[in] alpha_d the previously learned weights
+ * @param[in] rho_d the previously learned biases
+ * @param[in] sv_d the support vectors
+ * @param[in] predict_points_d the data points to predict
+ * @param[in] num_classes the number of classes
+ * @param[in] num_sv the number of support vectors
+ * @param[in] num_predict_points the number of data points to predict
+ * @param[in] num_features the number of features per data point
+ * @param[in] gamma the parameter in the rbf kernel function
+ */
+__global__ void device_kernel_predict_rbf(real_type *out_d, const real_type *alpha_d, const real_type *rho_d, const real_type *sv_d, const real_type *predict_points_d, const unsigned long long num_classes, const unsigned long long num_sv, const unsigned long long num_predict_points, const unsigned long long num_features, const real_type gamma) {
+    const unsigned long long sv_idx = blockIdx.x * blockDim.x + threadIdx.x;
+    const unsigned long long predict_points_idx = blockIdx.y * blockDim.y + threadIdx.y;
+    const unsigned long long class_idx = blockIdx.z * blockDim.z + threadIdx.z;
 
     if (sv_idx < num_sv && predict_points_idx < num_predict_points && class_idx < num_classes) {
         real_type temp{ 0.0 };
         // perform dot product
-        for (kernel_index_type dim = 0; dim < num_features; ++dim) {
+        for (unsigned long long dim = 0; dim < num_features; ++dim) {
             const real_type diff = sv_d[sv_idx * num_features + dim] - predict_points_d[predict_points_idx * num_features + dim];
             temp += diff * diff;
         }
