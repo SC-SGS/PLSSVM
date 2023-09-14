2dd4b5a804b2195c5543bf14de126092143091a2
Marcel Breyer <marcel.breyer@ipvs.uni-stuttgart.de>
2023-09-14 18:05:33 +0200

    Update formatting.

--- tests/backend_types.cpp
+++ tests/backend_types.cpp
@@ -13,17 +13,17 @@
 #include "plssvm/backends/SYCL/detail/constants.hpp"  // namespace plssvm::sycl
 #include "plssvm/detail/utility.hpp"                  // plssvm::detail::contains
 
-#include "custom_test_macros.hpp"                     // EXPECT_CONVERSION_TO_STRING, EXPECT_CONVERSION_FROM_STRING, EXPECT_THROW_WHAT
-#include "naming.hpp"                                 // naming::{pretty_print_unsupported_backend_combination, pretty_print_supported_backend_combination}
+#include "custom_test_macros.hpp"  // EXPECT_CONVERSION_TO_STRING, EXPECT_CONVERSION_FROM_STRING, EXPECT_THROW_WHAT
+#include "naming.hpp"              // naming::{pretty_print_unsupported_backend_combination, pretty_print_supported_backend_combination}
 
-#include "fmt/core.h"                                 // fmt::format
-#include "gmock/gmock.h"                              // EXPECT_THAT, ::testing::Contains
-#include "gtest/gtest.h"                              // TEST, EXPECT_EQ, EXPECT_TRUE, EXPECT_GE, TEST_P, INSTANTIATE_TEST_SUITE_P
+#include "fmt/core.h"     // fmt::format
+#include "gmock/gmock.h"  // EXPECT_THAT, ::testing::Contains
+#include "gtest/gtest.h"  // TEST, EXPECT_EQ, EXPECT_TRUE, EXPECT_GE, TEST_P, INSTANTIATE_TEST_SUITE_P
 
-#include <sstream>                                    // std::istringstream
-#include <tuple>                                      // std::tuple, std::ignore
-#include <utility>                                    // std::pair
-#include <vector>                                     // std::vector
+#include <sstream>  // std::istringstream
+#include <tuple>    // std::tuple, std::ignore
+#include <utility>  // std::pair
+#include <vector>   // std::vector
 
 // check whether the plssvm::backend_type -> std::string conversions are correct
 TEST(BackendType, to_string) {
