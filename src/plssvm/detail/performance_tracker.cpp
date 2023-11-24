c7d57df71b840c2a8bd127fa7a8b1103e0791bbc
Marcel Breyer <marcel.breyer@ipvs.uni-stuttgart.de>
2023-11-24 18:27:12 +0100

    Correctly track all padding sizes.

--- src/plssvm/detail/performance_tracker.cpp
+++ src/plssvm/detail/performance_tracker.cpp
@@ -8,7 +8,7 @@
 
 #include "plssvm/detail/performance_tracker.hpp"
 
-#include "plssvm/constants.hpp"                          // plssvm::real_type
+#include "plssvm/constants.hpp"                          // plssvm::real_type, plssvm::THREAD_BOCK_SIZE, plssvm::FEATURE_BLOCK_SIZE, plssvm::INTERNAL_BLOCK_SIZE, plssvm::THREAD_BLOCK_PADDING
 #include "plssvm/detail/arithmetic_type_name.hpp"        // plssvm::detail::arithmetic_type_name
 #include "plssvm/detail/assert.hpp"                      // PLSSVM_ASSERT, PLSSVM_ASSERT_ENABLED
 #include "plssvm/detail/cmd/parser_predict.hpp"          // plssvm::detail::cmd::parser_predict
@@ -171,8 +171,10 @@ void performance_tracker::save(std::ostream &out) {
         "  asserts:                    {}\n"
         "  gemm:                       {}\n"
         "  enforce_max_mem_alloc_size: {}\n"
-        "  PLSSVM_THREAD_BLOCK_SIZE:   {}\n"
-        "  PLSSVM_INTERNAL_BLOCK_SIZE: {}\n",
+        "  THREAD_BLOCK_SIZE:          {}\n"
+        "  FEATURE_BLOCK_SIZE:         {}\n"
+        "  INTERNAL_BLOCK_SIZE:        {}\n"
+        "  THREAD_BLOCK_PADDING:       {}\n",
         plssvm::detail::current_date_time(),
         version::detail::target_platforms,
         version::git_metadata::commit_sha1().empty() ? "unknown" : version::git_metadata::commit_sha1(),
@@ -185,7 +187,9 @@ void performance_tracker::save(std::ostream &out) {
         use_gemm,
         enforce_max_mem_alloc_size,
         THREAD_BLOCK_SIZE,
-        INTERNAL_BLOCK_SIZE);
+        FEATURE_BLOCK_SIZE,
+        INTERNAL_BLOCK_SIZE,
+        THREAD_BLOCK_PADDING);
 
 #if defined(PLSSVM_SYCL_BACKEND_HAS_DPCPP)
     //  check whether DPC++ AOT has been enabled
