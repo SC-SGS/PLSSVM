ffd270828c07cffa5550ef006ae972a1e53f13dd
Marcel Breyer <marcel.breyer@ipvs.uni-stuttgart.de>
2023-12-18 17:16:46 +0100

    Add missing documentation.

--- include/plssvm/exceptions/exceptions.hpp
+++ include/plssvm/exceptions/exceptions.hpp
@@ -173,8 +173,16 @@ class matrix_exception : public exception {
     explicit matrix_exception(const std::string &msg, source_location loc = source_location::current());
 };
 
+/**
+ * @brief Exception type thrown if a kernel launch requests more resources than are available in any of our backends.
+ */
 class kernel_launch_resources : public exception {
   public:
+    /**
+     * @brief Construct a new exception forwarding the exception message and source location to plssvm::exception.
+     * @param[in] msg the exception's `what()` message
+     * @param[in] loc the exception's call side information
+     */
     explicit kernel_launch_resources(const std::string &msg, source_location loc = source_location::current());
 };
 
