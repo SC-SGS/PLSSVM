3dea00c1f0bf6ff67c460be43c87cf97dcc07946
Marcel Breyer <marcel.breyer@ipvs.uni-stuttgart.de>
2024-05-13 15:32:40 +0200

    Fix wrong file name in tests with icpx.

--- tests/backends/generic_exceptions_tests.hpp
+++ tests/backends/generic_exceptions_tests.hpp
@@ -52,7 +52,7 @@ TYPED_TEST_P(Exception, exception_source_location) {
 
     const exception_type exc = dummy<exception_type>("exception message");
 
-    EXPECT_EQ(exc.loc().file_name(), __FILE__);
+    EXPECT_EQ(exc.loc().file_name(), "generic_exceptions_tests.hpp");
     EXPECT_THAT(exc.loc().function_name(), ::testing::HasSubstr("dummy"));
     EXPECT_EQ(exc.loc().line(), std::uint_least32_t{ 32 });   // attention: hardcoded line!
     EXPECT_EQ(exc.loc().column(), std::uint_least32_t{ 0 });  // attention: always 0!
@@ -75,7 +75,7 @@ TYPED_TEST_P(Exception, exception_what_with_source_location) {
     // check the "what" message content
     EXPECT_EQ(what_lines[0], "exception message");
     EXPECT_EQ(what_lines[1], fmt::format("{} thrown:", exception_name));
-    EXPECT_EQ(what_lines[2], "  in file      " __FILE__);
+    EXPECT_EQ(what_lines[2], "  in file      generic_exceptions_tests.hpp");
     EXPECT_THAT(std::string{ what_lines[3] }, ::testing::ContainsRegex("  in function  .*dummy.*"));
     EXPECT_EQ(what_lines[4], "  @ line       32");
 }
