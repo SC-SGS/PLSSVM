e5adf540d882b726690df7e14c194c5fe2ab43b7
Marcel Breyer <marcel.breyer@ipvs.uni-stuttgart.de>
2024-05-13 16:18:31 +0200

    Explicitly use std::string instead of c-style string literals.

--- tests/backends/generic_exceptions_tests.hpp
+++ tests/backends/generic_exceptions_tests.hpp
@@ -52,7 +52,7 @@ TYPED_TEST_P(Exception, exception_source_location) {
 
     const exception_type exc = dummy<exception_type>("exception message");
 
-    EXPECT_EQ(exc.loc().file_name(), "generic_exceptions_tests.hpp");
+    EXPECT_EQ(exc.loc().file_name(), std::string{ "generic_exceptions_tests.hpp" });
     EXPECT_THAT(exc.loc().function_name(), ::testing::HasSubstr("dummy"));
     EXPECT_EQ(exc.loc().line(), std::uint_least32_t{ 32 });   // attention: hardcoded line!
     EXPECT_EQ(exc.loc().column(), std::uint_least32_t{ 0 });  // attention: always 0!
@@ -73,11 +73,11 @@ TYPED_TEST_P(Exception, exception_what_with_source_location) {
     ASSERT_EQ(what_lines.size(), 5);
 
     // check the "what" message content
-    EXPECT_EQ(what_lines[0], "exception message");
+    EXPECT_EQ(what_lines[0], std::string{ "exception message" });
     EXPECT_EQ(what_lines[1], fmt::format("{} thrown:", exception_name));
-    EXPECT_EQ(what_lines[2], "  in file      generic_exceptions_tests.hpp");
+    EXPECT_EQ(what_lines[2], std::string{ "  in file      generic_exceptions_tests.hpp" });
     EXPECT_THAT(std::string{ what_lines[3] }, ::testing::ContainsRegex("  in function  .*dummy.*"));
-    EXPECT_EQ(what_lines[4], "  @ line       32");
+    EXPECT_EQ(what_lines[4], std::string{ "  @ line       32" });
 }
 
 REGISTER_TYPED_TEST_SUITE_P(Exception,
