9edfae27f1830768d384c1d4d53069bf98103656
Marcel Breyer <marcel.breyer@ipvs.uni-stuttgart.de>
2024-07-05 10:43:53 +0200

    Add CMake sanity checks for natural numbers.

--- cmake/utility_macros.cmake
+++ cmake/utility_macros.cmake
@@ -6,18 +6,25 @@
 
 # set variable in the local and parent scope
 macro(set_local_and_parent NAME VALUE)
-    if(${ARGC} GREATER 2)
+    if (${ARGC} GREATER 2)
         set(PLSSVM_TEMP_ARGN "${ARGN}")
         list(JOIN PLSSVM_TEMP_ARGN " " PLSSVM_REMAINING_FLAGS)
         set(${ARGV0} "${ARGV1} ${PLSSVM_REMAINING_FLAGS}")
         set(${ARGV0} "${ARGV1} ${PLSSVM_REMAINING_FLAGS}" PARENT_SCOPE)
-    else()
+    else ()
         set(${ARGV0} "${ARGV1}")
         set(${ARGV0} "${ARGV1}" PARENT_SCOPE)
-    endif()
+    endif ()
 endmacro()
 
 macro(append_local_and_parent LIST_NAME VALUE)
     list(APPEND ${ARGV0} ${ARGV1})
     set(${ARGV0} ${${ARGV0}} PARENT_SCOPE)
+endmacro()
+
+# test whether the provided variable contains a natural number greater than zero
+macro(check_integer VARIABLE)
+    if (NOT ${${VARIABLE}} MATCHES "^[0-9]+$" OR ${${VARIABLE}} LESS_EQUAL 0)
+        message(FATAL_ERROR "The ${VARIABLE} must be a natural number greater 0, but is \"${${VARIABLE}}\"!")
+    endif ()
 endmacro()
