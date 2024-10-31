c7321ee65ca8f17faadfec353d999dc5021947f0
Marcel Breyer <marcel.breyer@ipvs.uni-stuttgart.de>
2024-10-31 16:17:45 +0100

    Fix wrong documentation.

--- include/plssvm/backends/SYCL/kernel/predict_kernel.hpp
+++ include/plssvm/backends/SYCL/kernel/predict_kernel.hpp
@@ -258,7 +258,7 @@ class device_kernel_predict_linear {
 /**
  * @brief Predict the @p predict_points_d using the @p kernel_function.
  * @tparam kernel_function the type of the used kernel function
- * @tparam Args the types of the parameters necessary for the specific kernel function
+ * @tparam Args the types of the parameters necessary for the specific kernel function; stored in a `std::tuple`
  */
 template <kernel_function_type kernel_function, typename... Args>
 class device_kernel_predict {
