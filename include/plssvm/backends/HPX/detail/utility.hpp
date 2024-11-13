124a779e71a375dc7a2dccba97deaf5a2550ae1f
constracktor <strack.as@icloud.com>
2024-11-13 19:29:17 +0100

    Initial commit HPX backend

---
+++ include/plssvm/backends/HPX/detail/utility.hpp
@@ -0,0 +1,33 @@
+/**
+ * @file
+ * @author Alexander Van Craen
+ * @author Marcel Breyer
+ * @author Alexander Strack
+ * @copyright 2018-today The PLSSVM project - All Rights Reserved
+ * @license This file is part of the PLSSVM project which is released under the MIT license.
+ *          See the LICENSE.md file in the project root for full license information.
+ *
+ * @brief Utility functions specific to the HPX backend.
+ */
+
+#ifndef PLSSVM_BACKENDS_HPX_DETAIL_UTILITY_HPP_
+#define PLSSVM_BACKENDS_HPX_DETAIL_UTILITY_HPP_
+#pragma once
+
+    #include "boost/atomic/atomic_ref.hpp"  // boost::atomic_ref
+
+#include <string>  // std::string
+
+namespace plssvm::hpx::detail {
+
+using boost::atomic_ref;
+
+/**
+ * @brief Return the version of the HPX backend.
+ * @return the HPX version (`[[nodiscard]]`)
+ */
+// [[nodiscard]] std::string get_hpx_version();
+
+}  // namespace plssvm::hpx::detail
+
+#endif  // PLSSVM_BACKENDS_HPX_DETAIL_UTILITY_HPP_
