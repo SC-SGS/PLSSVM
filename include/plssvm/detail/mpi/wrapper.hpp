258acd065be59a590269324691d416b4ae936d9b
Marcel Breyer <marcel.breyer@ipvs.uni-stuttgart.de>
2024-12-05 16:53:44 +0100

    Create MPI dummy header and implementation files.

---
+++ include/plssvm/detail/mpi/wrapper.hpp
@@ -0,0 +1,21 @@
+/**
+ * @file
+ * @author Alexander Van Craen
+ * @author Marcel Breyer
+ * @copyright 2018-today The PLSSVM project - All Rights Reserved
+ * @license This file is part of the PLSSVM project which is released under the MIT license.
+ *          See the LICENSE.md file in the project root for full license information.
+ *
+ * @brief Defines some wrapper functions necessary for our MPI support.
+ * @details These wrapper functions are only conditionally compiled such that MPI is still an **optional** dependency in PLSSVM.
+*/
+
+#ifndef PLSSVM_DETAIL_MPI_WRAPPER_HPP_
+#define PLSSVM_DETAIL_MPI_WRAPPER_HPP_
+#pragma once
+
+namespace plssvm::detail::mpi {
+
+}
+
+#endif  // PLSSVM_DETAIL_MPI_WRAPPER_HPP_
---
+++ src/plssvm/detail/mpi/wrapper.cpp
@@ -0,0 +1,5 @@
+#include "plssvm/detail/mpi/wrapper.hpp"
+
+namespace plssvm::detail::mpi {
+
+}
