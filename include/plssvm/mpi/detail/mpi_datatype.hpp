2bacd783ad7d29d93f478a00bd5e988dc62c9aaa
Marcel Breyer <marcel.breyer@ipvs.uni-stuttgart.de>
2024-12-10 09:39:22 +0100

    Create function to map C++ datatypes to MPI datatypes.

---
+++ include/plssvm/mpi/detail/mpi_datatype.hpp
@@ -0,0 +1,65 @@
+/**
+ * @file
+ * @author Alexander Van Craen
+ * @author Marcel Breyer
+ * @copyright 2018-today The PLSSVM project - All Rights Reserved
+ * @license This file is part of the PLSSVM project which is released under the MIT license.
+ *          See the LICENSE.md file in the project root for full license information.
+ *
+ * @brief Convert a provided type to the corresponding MPI_Datatype, if possible.
+ */
+
+#ifndef PLSSVM_MPI_DETAIL_MPI_DATATYPE_HPP_
+#define PLSSVM_MPI_DETAIL_MPI_DATATYPE_HPP_
+#pragma once
+
+#if defined(PLSSVM_HAS_MPI_ENABLED)
+
+    #include "mpi.h"  // MPI_Datatype, various MPI datatypes
+
+    #include <complex>  // std::complex
+
+    #define PLSSVM_CREATE_MPI_DATATYPE_MAPPING(cpp_type, mpi_type) \
+        template <>                                                \
+        [[nodiscard]] inline MPI_Datatype mpi_datatype<cpp_type>() { return mpi_type; }
+
+namespace plssvm::mpi::detail {
+
+template <typename T>
+[[nodiscard]] inline MPI_Datatype mpi_datatype() = delete;
+
+PLSSVM_CREATE_MPI_DATATYPE_MAPPING(char, MPI_CHAR)
+PLSSVM_CREATE_MPI_DATATYPE_MAPPING(signed char, MPI_SIGNED_CHAR)
+PLSSVM_CREATE_MPI_DATATYPE_MAPPING(unsigned char, MPI_UNSIGNED_CHAR)
+PLSSVM_CREATE_MPI_DATATYPE_MAPPING(wchar_t, MPI_WCHAR)
+PLSSVM_CREATE_MPI_DATATYPE_MAPPING(signed short, MPI_SHORT)
+PLSSVM_CREATE_MPI_DATATYPE_MAPPING(unsigned short, MPI_UNSIGNED_SHORT)
+PLSSVM_CREATE_MPI_DATATYPE_MAPPING(signed int, MPI_INT)
+PLSSVM_CREATE_MPI_DATATYPE_MAPPING(unsigned int, MPI_UNSIGNED)
+PLSSVM_CREATE_MPI_DATATYPE_MAPPING(signed long int, MPI_LONG)
+PLSSVM_CREATE_MPI_DATATYPE_MAPPING(unsigned long int, MPI_UNSIGNED_LONG)
+PLSSVM_CREATE_MPI_DATATYPE_MAPPING(signed long long int, MPI_LONG_LONG)
+PLSSVM_CREATE_MPI_DATATYPE_MAPPING(unsigned long long int, MPI_UNSIGNED_LONG_LONG)
+PLSSVM_CREATE_MPI_DATATYPE_MAPPING(float, MPI_FLOAT)
+PLSSVM_CREATE_MPI_DATATYPE_MAPPING(double, MPI_DOUBLE)
+PLSSVM_CREATE_MPI_DATATYPE_MAPPING(long double, MPI_LONG_DOUBLE)
+// PLSSVM_CREATE_MPI_DATATYPE_MAPPING(std::int8_t, MPI_INT8_T)
+// PLSSVM_CREATE_MPI_DATATYPE_MAPPING(std::int16_t, MPI_INT16_T)
+// PLSSVM_CREATE_MPI_DATATYPE_MAPPING(std::int32_t, MPI_INT32_T)
+// PLSSVM_CREATE_MPI_DATATYPE_MAPPING(std::int64_t, MPI_INT64_T)
+// PLSSVM_CREATE_MPI_DATATYPE_MAPPING(std::uint8_t, MPI_UINT8_T)
+// PLSSVM_CREATE_MPI_DATATYPE_MAPPING(std::uint16_t, MPI_UINT16_T)
+// PLSSVM_CREATE_MPI_DATATYPE_MAPPING(std::uint32_t, MPI_UINT32_T)
+// PLSSVM_CREATE_MPI_DATATYPE_MAPPING(std::uint64_t, MPI_UINT64_T)
+PLSSVM_CREATE_MPI_DATATYPE_MAPPING(bool, MPI_C_BOOL)
+PLSSVM_CREATE_MPI_DATATYPE_MAPPING(std::complex<float>, MPI_C_COMPLEX)
+PLSSVM_CREATE_MPI_DATATYPE_MAPPING(std::complex<double>, MPI_C_DOUBLE_COMPLEX)
+PLSSVM_CREATE_MPI_DATATYPE_MAPPING(std::complex<long double>, MPI_C_LONG_DOUBLE_COMPLEX)
+
+}  // namespace plssvm::mpi::detail
+
+    #undef PLSSVM_CREATE_MPI_DATATYPE_MAPPING
+
+#endif
+
+#endif  // PLSSVM_MPI_DETAIL_MPI_DATATYPE_HPP_
