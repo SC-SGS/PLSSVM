48256b2b5e286aab3797e75d8c7f4b41ac3f0ce4
Marcel Breyer <marcel.breyer@ipvs.uni-stuttgart.de>
2025-01-22 11:35:55 +0100

    Change sklearn like examples from completely artificial to better examples.

--- examples/python/sklearn_like_svr.py
+++ examples/python/sklearn_like_svr.py
@@ -1,17 +1,58 @@
-from sklearn.datasets import make_regression
-import plssvm
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-num_samples = 2**8
-num_features = 2**6
+########################################################################################################################
+# Authors: Alexander Van Craen, Marcel Breyer                                                                          #
+# Copyright (C): 2018-today The PLSSVM project - All Rights Reserved                                                   #
+# License: This file is part of the PLSSVM project which is released under the MIT license.                            #
+#          See the LICENSE.md file in the project root for full license information.                                   #
+########################################################################################################################
 
-samples, labels = make_regression(n_samples=num_samples, n_features=num_features, noise=30)
+import numpy as np
+import matplotlib.pyplot as plt
 
-# create a C-SVR
-svr = plssvm.SVR(kernel='linear', C=1.0, tol=1e-3, verbose=False)
+# generate sample data (sine curve with noise)
+X = np.sort(5 * np.random.rand(40, 1), axis=0)
+y = np.sin(X).ravel()
 
-# fit the model
-svr.fit(samples, labels)
+# add noise to targets
+y[::5] += 3 * (0.5 - np.random.rand(8))
 
-# score the data set
-model_accuracy = svr.score(samples, labels)
-print("model accuracy (R^2): {0:.2f}".format(model_accuracy * 100))
+plt.scatter(X, y, color='darkorange', label='data')
+
+# fit the sklearn regression model
+from sklearn.svm import SVR
+
+sklearn_svr_lin = SVR(kernel='linear', C=100, epsilon=0.1)
+y_lin_sklearn = sklearn_svr_lin.fit(X, y).predict(X)
+plt.plot(X, y_lin_sklearn, lw=2, linestyle='dashed', label='Linear model sklearn')
+
+sklearn_svr_poly = SVR(kernel='poly', C=100, degree=3, epsilon=0.1, coef0=1)
+y_poly_sklearn = sklearn_svr_poly.fit(X, y).predict(X)
+plt.plot(X, y_poly_sklearn, lw=2, linestyle='dashed', label='Polynomial model sklearn')
+
+sklearn_svr_rbf = SVR(kernel='rbf', C=100, gamma=0.1, epsilon=0.1)
+y_rbf_sklearn = sklearn_svr_rbf.fit(X, y).predict(X)
+plt.plot(X, y_rbf_sklearn, lw=2, linestyle='dashed', label='RBF model sklearn')
+
+# fit the PLSSVM regression model
+from plssvm import SVR
+
+plssvm_svr_lin = SVR(kernel='linear', C=100)
+y_lin_plssvm = plssvm_svr_lin.fit(X, y).predict(X)
+plt.plot(X, y_lin_plssvm, lw=2, label='Linear model plssvm')
+
+plssvm_svr_poly = SVR(kernel='poly', C=100, degree=3, coef0=1)
+y_poly_plssvm = plssvm_svr_poly.fit(X, y).predict(X)
+plt.plot(X, y_poly_plssvm, lw=2, label='Polynomial model plssvm')
+
+plssvm_svr_rbf = SVR(kernel='rbf', C=100, gamma=0.1)
+y_rbf_plssvm = plssvm_svr_rbf.fit(X, y).predict(X)
+plt.plot(X, y_rbf_plssvm, lw=2, label='RBF model plssvm')
+
+# show the result plots
+plt.xlabel('data')
+plt.ylabel('target')
+plt.title('Support Vector Regression')
+plt.legend()
+plt.show()
