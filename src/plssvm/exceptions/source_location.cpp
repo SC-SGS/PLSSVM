ccaec109c9aebb39f6bfdb3860ff148dc2cd8428
Marcel Breyer <marcel.breyer@ipvs.uni-stuttgart.de>
2025-02-23 18:40:06 +0100

    Do not use the mpi::communicator constructor to retrieve the current MPI rank due to excessive calls.

--- src/plssvm/exceptions/source_location.cpp
+++ src/plssvm/exceptions/source_location.cpp
@@ -8,8 +8,10 @@
 
 #include "plssvm/exceptions/source_location.hpp"
 
-#include "plssvm/mpi/communicator.hpp"  // plssvm::mpi::communicator
-#include "plssvm/mpi/environment.hpp"   // plssvm::mpi::is_active
+#if defined(PLSSVM_HAS_MPI_ENABLED)
+    #include "mpi.h"  // MPI_Comm_rank, MPI_COMM_WORLD
+#endif
+#include "plssvm/mpi/environment.hpp"  // plssvm::mpi::is_active
 
 #include <cstdint>   // std::uint_least32_t
 #include <optional>  // std::make_optional
@@ -27,7 +29,12 @@ source_location source_location::current(const char *file_name, const char *func
     // try getting the MPI rank wrt to MPI_COMM_WORLD
     try {
         if (mpi::is_active()) {
-            loc.world_rank_ = std::make_optional(mpi::communicator{}.rank());
+            // prevent excessive mpi::communicator constructor calls
+#if defined(PLSSVM_HAS_MPI_ENABLED)
+            int rank;
+            MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+            loc.world_rank_ = std::make_optional(rank);
+#endif
         }
     } catch (...) {
         // std::nullopt
