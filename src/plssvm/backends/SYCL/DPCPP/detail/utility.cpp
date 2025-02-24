93fc69eb4d45dd1288007bbfa06c6e2c83412046
Alexander Van Craen <Alexander.Van-Craen@ipvs.uni-stuttgart.de>
2025-02-24 14:35:20 +0100

    add exception header inclusion for platform device checks in utility files

--- src/plssvm/backends/SYCL/DPCPP/detail/utility.cpp
+++ src/plssvm/backends/SYCL/DPCPP/detail/utility.cpp
@@ -12,6 +12,7 @@
 #include "plssvm/backends/SYCL/DPCPP/detail/queue_impl.hpp"  // plssvm::dpcpp::detail::queue (PImpl implementation)
 #include "plssvm/detail/string_utility.hpp"                  // plssvm::detail::{as_lower_case, contains}
 #include "plssvm/detail/utility.hpp"                         // plssvm::detail::contains
+#include "plssvm/exceptions/exceptions.hpp"                  // plssvm::platform_devices_empty
 #include "plssvm/target_platforms.hpp"                       // plssvm::target_platform, plssvm::determine_default_target_platform
 
 #include "sycl/sycl.hpp"  // ::sycl::platform, ::sycl::device, ::sycl::property::queue, ::sycl::info
