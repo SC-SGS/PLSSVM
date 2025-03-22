76967611cf046a49275280c15e1f768b53a1dd78
Marcel Breyer <marcel.breyer@ipvs.uni-stuttgart.de>
2025-03-22 01:10:19 +0100

    Add additional compiler flags for the code coverage.

--- cmake/add_coverage_build_type.cmake
+++ cmake/add_coverage_build_type.cmake
@@ -4,15 +4,23 @@
 #          See the LICENSE.md file in the project root for full license information.
 ########################################################################################################################
 
+# define the used compiler and linker flags for the coverage target
+set(PLSSVM_COVERAGE_COMPILER_FLAGS "-O0 -g --coverage -fprofile-abs-path -fno-inline -fno-inline-functions -fno-inline-small-functions -fno-elide-constructors -fno-common -ffunction-sections -fno-omit-frame-pointer")
+set(PLSSVM_COVERAGE_LINKER_FLAGS "-O0 -g --coverage -fno-lto -lgcov")
+set(CMAKE_INTERPROCEDURAL_OPTIMIZATION OFF CACHE BOOL "" FORCE)
+
+# also enable code coverage for nvcc
+set(CMAKE_CUDA_FLAGS "-Xcompiler '-O0 -g --coverage'")
+
 # add new coverage build type
-set(CMAKE_CXX_FLAGS_COVERAGE "${CMAKE_CXX_FLAGS_DEBUG} -fprofile-arcs -ftest-coverage" CACHE STRING "Flags used by the C++ compiler during coverage builds."
+set(CMAKE_CXX_FLAGS_COVERAGE "${CMAKE_CXX_FLAGS_DEBUG} ${PLSSVM_COVERAGE_COMPILER_FLAGS}" CACHE STRING "Flags used by the C++ compiler during coverage builds."
                                                                                              FORCE
 )
-set(CMAKE_C_FLAGS_COVERAGE "${CMAKE_C_FLAGS_DEBUG} -fprofile-arcs -ftest-coverage" CACHE STRING "Flags used by the C compiler during coverage builds." FORCE)
-set(CMAKE_EXE_LINKER_FLAGS_COVERAGE "${CMAKE_EXE_LINKER_FLAGS_DEBUG} -fprofile-arcs -ftest-coverage -lgcov"
+set(CMAKE_C_FLAGS_COVERAGE "${CMAKE_C_FLAGS_DEBUG} ${PLSSVM_COVERAGE_COMPILER_FLAGS}" CACHE STRING "Flags used by the C compiler during coverage builds." FORCE)
+set(CMAKE_EXE_LINKER_FLAGS_COVERAGE "${CMAKE_EXE_LINKER_FLAGS_DEBUG} ${PLSSVM_COVERAGE_LINKER_FLAGS} -lgcov"
     CACHE STRING "Flags used for linking binaries during coverage builds." FORCE
 )
-set(CMAKE_SHARED_LINKER_FLAGS_COVERAGE "${CMAKE_SHARED_LINKER_FLAGS_DEBUG} -fprofile-arcs -ftest-coverage -lgcov"
+set(CMAKE_SHARED_LINKER_FLAGS_COVERAGE "${CMAKE_SHARED_LINKER_FLAGS_DEBUG} ${PLSSVM_COVERAGE_LINKER_FLAGS} -lgcov"
     CACHE STRING "Flags used by the shared libraries linker during coverage builds." FORCE
 )
 mark_as_advanced(CMAKE_CXX_FLAGS_COVERAGE CMAKE_C_FLAGS_COVERAGE CMAKE_EXE_LINKER_FLAGS_COVERAGE CMAKE_SHARED_LINKER_FLAGS_COVERAGE)
