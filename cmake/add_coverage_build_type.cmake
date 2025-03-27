0697deeec6634664c21d5f73d2e59de3a8f6391c
Marcel Breyer <marcel.breyer@ipvs.uni-stuttgart.de>
2025-03-27 15:14:34 +0100

    Fix CMake format.

--- cmake/add_coverage_build_type.cmake
+++ cmake/add_coverage_build_type.cmake
@@ -5,13 +5,15 @@
 ########################################################################################################################
 
 # define the used compiler and linker flags for the coverage target
-set(PLSSVM_COVERAGE_COMPILER_FLAGS "-O0 -g --coverage -fprofile-abs-path -fno-inline -fno-inline-functions -fno-inline-small-functions -fno-elide-constructors -fno-common -ffunction-sections -fno-omit-frame-pointer")
+set(PLSSVM_COVERAGE_COMPILER_FLAGS
+    "-O0 -g --coverage -fprofile-abs-path -fno-inline -fno-inline-functions -fno-inline-small-functions -fno-elide-constructors -fno-common -ffunction-sections -fno-omit-frame-pointer"
+)
 set(PLSSVM_COVERAGE_LINKER_FLAGS "-O0 -g --coverage -fno-lto -lgcov")
 set(CMAKE_INTERPROCEDURAL_OPTIMIZATION OFF CACHE BOOL "" FORCE)
 
 # add new coverage build type
 set(CMAKE_CXX_FLAGS_COVERAGE "${CMAKE_CXX_FLAGS_DEBUG} ${PLSSVM_COVERAGE_COMPILER_FLAGS}" CACHE STRING "Flags used by the C++ compiler during coverage builds."
-                                                                                             FORCE
+                                                                                                FORCE
 )
 set(CMAKE_C_FLAGS_COVERAGE "${CMAKE_C_FLAGS_DEBUG} ${PLSSVM_COVERAGE_COMPILER_FLAGS}" CACHE STRING "Flags used by the C compiler during coverage builds." FORCE)
 set(CMAKE_EXE_LINKER_FLAGS_COVERAGE "${CMAKE_EXE_LINKER_FLAGS_DEBUG} ${PLSSVM_COVERAGE_LINKER_FLAGS} -lgcov"
