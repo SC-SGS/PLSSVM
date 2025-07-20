9944b7de02ebae36b8f12ff0c95103cc374b2c7c
Marcel Breyer <marcel.breyer@ipvs.uni-stuttgart.de>
2025-07-20 12:15:02 +0200

    Add support for the new solver type to the Python bindings.

--- bindings/Python/solver_types.cpp
+++ bindings/Python/solver_types.cpp
@@ -20,6 +20,7 @@ void init_solver_types(py::module_ &m) {
     py_enum
         .value("AUTOMATIC", plssvm::solver_type::automatic, "the default solver type; depends on the available device and system memory")
         .value("CG_EXPLICIT", plssvm::solver_type::cg_explicit, "explicitly assemble the kernel matrix on the device")
+        .value("CG_STREAMING", plssvm::solver_type::cg_streaming, "explicitly calculate the kernel matrix and fully store it on the host; realized using unified shared memory")
         .value("CG_IMPLICIT", plssvm::solver_type::cg_implicit, "implicitly calculate the kernel matrix entries in each CG iteration");
 
     // enable implicit conversion from string to enum
