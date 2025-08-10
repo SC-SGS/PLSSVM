c6eb56b5b8ebf8771817cc2de38d6a30e635fa5c
Marcel Breyer <marcel.breyer@ipvs.uni-stuttgart.de>
2025-08-10 18:52:38 +0200

    Fix error when using rocstdpar on AMD GPUs.

--- include/plssvm/backends/stdpar/kernel/predict_kernel.hpp
+++ include/plssvm/backends/stdpar/kernel/predict_kernel.hpp
@@ -70,8 +70,8 @@ struct device_kernel_w_linear {
 
         std::for_each(std::execution::par_unseq, range.begin(), range.end(), [=, w_ptr = w.data(), alpha_ptr = alpha.data(), support_vectors_ptr = support_vectors.data()](const std::size_t idx) {
             // calculate the indices used in the current thread
-            const std::size_t feature_idx = (idx % blocked_num_classes) * INTERNAL_BLOCK_SIZE_uz;
-            const std::size_t class_idx = (idx / blocked_num_classes) * INTERNAL_BLOCK_SIZE_uz;
+            const std::size_t feature_idx = (idx / blocked_num_classes) * INTERNAL_BLOCK_SIZE_uz;
+            const std::size_t class_idx = (idx % blocked_num_classes) * INTERNAL_BLOCK_SIZE_uz;
 
             // create a thread private array used for internal caching
             std::array<std::array<real_type, INTERNAL_BLOCK_SIZE>, INTERNAL_BLOCK_SIZE> temp{};
@@ -165,8 +165,8 @@ struct device_kernel_predict_linear {
 
         std::for_each(std::execution::par_unseq, range.begin(), range.end(), [=, prediction_ptr = prediction.data(), w_ptr = w.data(), rho_ptr = rho.data(), predict_points_ptr = predict_points.data()](const std::size_t idx) {
             // calculate the indices used in the current thread
-            const std::size_t pp_idx = (idx % blocked_num_classes) * INTERNAL_BLOCK_SIZE_uz;
-            const std::size_t class_idx = (idx / blocked_num_classes) * INTERNAL_BLOCK_SIZE_uz;
+            const std::size_t pp_idx = (idx / blocked_num_classes) * INTERNAL_BLOCK_SIZE_uz;
+            const std::size_t class_idx = (idx % blocked_num_classes) * INTERNAL_BLOCK_SIZE_uz;
 
             // create a thread private array used for internal caching
             std::array<std::array<real_type, INTERNAL_BLOCK_SIZE>, INTERNAL_BLOCK_SIZE> temp{};
