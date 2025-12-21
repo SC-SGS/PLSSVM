3cd4b1610561d6c02394bff7da3c340fa40f130f
Marcel Breyer <marcel.breyer@ipvs.uni-stuttgart.de>
2025-12-21 13:42:51 +0100

    Add necessary pickle support to the new Tag classes.

--- bindings/Python/sklearn_like/tags.cpp
+++ bindings/Python/sklearn_like/tags.cpp
@@ -46,7 +46,30 @@ void init_sklearn_tags(py::module_ &m) {
                 bool_as_python_string(t.positive_only),
                 bool_as_python_string(t.multi_output),
                 bool_as_python_string(t.single_output));
-        });
+        })
+        .def(py::pickle(
+            // clang-format off
+            [](const TargetTags &self) {   // __getstate__
+                // return a tuple that fully encodes the state of the object
+                return py::make_tuple(self.required, self.one_d_labels, self.two_d_labels, self.positive_only, self.multi_output, self.single_output);
+            },
+            [](py::tuple t) {  // NOLINT: __setstate__
+                if (t.size() != 6) {
+                    throw std::runtime_error{ "Invalid TargetTags pickle state" };
+                }
+                // create a new C++ instance
+                TargetTags tags;
+                tags.required = t[0].cast<bool>();
+                tags.one_d_labels = t[1].cast<bool>();
+                tags.two_d_labels = t[2].cast<bool>();
+                tags.positive_only = t[3].cast<bool>();
+                tags.multi_output = t[4].cast<bool>();
+                tags.single_output = t[5].cast<bool>();
+                return tags;
+            }
+            )
+             // clang-format on
+        );
 
     // TransformerTags
     py::class_<TransformerTags>(m, "TransformerTags")
@@ -56,7 +79,25 @@ void init_sklearn_tags(py::module_ &m) {
             return fmt::format(
                 "TransformerTags(preserves_dtype=[{}])",
                 fmt::join(t.preserves_dtype, ", "));
-        });
+        })
+        .def(py::pickle(
+            // clang-format off
+            [](const TransformerTags &self) {   // __getstate__
+                // return a tuple that fully encodes the state of the object
+                return py::make_tuple(self.preserves_dtype);
+            },
+            [](py::tuple t) {  // NOLINT: __setstate__
+                if (t.size() != 1) {
+                    throw std::runtime_error{ "Invalid TransformerTags pickle state" };
+                }
+                // create a new C++ instance
+                TransformerTags tags;
+                tags.preserves_dtype = t[0].cast<std::vector<std::string>>();
+                return tags;
+            }
+            )
+             // clang-format on
+        );
 
     // ClassifierTags
     py::class_<ClassifierTags>(m, "ClassifierTags")
@@ -70,7 +111,27 @@ void init_sklearn_tags(py::module_ &m) {
                 bool_as_python_string(t.poor_score),
                 bool_as_python_string(t.multi_class),
                 bool_as_python_string(t.multi_label));
-        });
+        })
+        .def(py::pickle(
+            // clang-format off
+            [](const ClassifierTags &self) {   // __getstate__
+                // return a tuple that fully encodes the state of the object
+                return py::make_tuple(self.poor_score, self.multi_class, self.multi_label);
+            },
+            [](py::tuple t) {  // NOLINT: __setstate__
+                if (t.size() != 3) {
+                    throw std::runtime_error{ "Invalid ClassifierTags pickle state" };
+                }
+                // create a new C++ instance
+                ClassifierTags tags;
+                tags.poor_score = t[0].cast<bool>();
+                tags.multi_class = t[1].cast<bool>();
+                tags.multi_label = t[2].cast<bool>();
+                return tags;
+            }
+            )
+             // clang-format on
+        );
 
     // RegressorTags
     py::class_<RegressorTags>(m, "RegressorTags")
@@ -80,7 +141,25 @@ void init_sklearn_tags(py::module_ &m) {
             return fmt::format(
                 "RegressorTags(poor_score={})",
                 bool_as_python_string(t.poor_score));
-        });
+        })
+        .def(py::pickle(
+            // clang-format off
+            [](const RegressorTags &self) {   // __getstate__
+                // return a tuple that fully encodes the state of the object
+                return py::make_tuple(self.poor_score);
+            },
+            [](py::tuple t) {  // NOLINT: __setstate__
+                if (t.size() != 1) {
+                    throw std::runtime_error{ "Invalid RegressorTags pickle state" };
+                }
+                // create a new C++ instance
+                RegressorTags tags;
+                tags.poor_score = t[0].cast<bool>();
+                return tags;
+            }
+            )
+             // clang-format on
+        );
 
     // InputTags
     py::class_<InputTags>(m, "InputTags")
@@ -110,7 +189,35 @@ void init_sklearn_tags(py::module_ &m) {
                 bool_as_python_string(t.positive_only),
                 bool_as_python_string(t.allow_nan),
                 bool_as_python_string(t.pairwise));
-        });
+        })
+        .def(py::pickle(
+            // clang-format off
+            [](const InputTags &self) {   // __getstate__
+                // return a tuple that fully encodes the state of the object
+                return py::make_tuple(self.one_d_array, self.two_d_array, self.three_d_array, self.sparse, self.categorical,
+                                      self.string, self.dict, self.positive_only, self.allow_nan, self.pairwise);
+            },
+            [](py::tuple t) {  // NOLINT: __setstate__
+                if (t.size() != 10) {
+                    throw std::runtime_error{ "Invalid InputTags pickle state" };
+                }
+                // create a new C++ instance
+                InputTags tags;
+                tags.one_d_array = t[0].cast<bool>();
+                tags.two_d_array = t[1].cast<bool>();
+                tags.three_d_array = t[2].cast<bool>();;
+                tags.sparse = t[3].cast<bool>();
+                tags.categorical = t[4].cast<bool>();
+                tags.string = t[5].cast<bool>();
+                tags.dict = t[6].cast<bool>();
+                tags.positive_only = t[7].cast<bool>();
+                tags.allow_nan = t[8].cast<bool>();
+                tags.pairwise = t[9].cast<bool>();
+                return tags;
+            }
+            )
+             // clang-format on
+        );
 
     // Tags (root object)
     py::class_<Tags>(m, "Tags")
@@ -153,5 +260,34 @@ void init_sklearn_tags(py::module_ &m) {
                 bool_as_python_string(t.requires_fit),
                 bool_as_python_string(t._skip_test),
                 py::repr(py::cast(t.input_tags)).cast<std::string>());
-        });
+        })
+        .def(py::pickle(
+            // clang-format off
+            [](const Tags &self) {   // __getstate__
+                // return a tuple that fully encodes the state of the object
+                return py::make_tuple(self.estimator_type, self.target_tags, self.transformer_tags, self.classifier_tags, self.regressor_tags,
+                                      self.array_api_support, self.no_validation, self.non_deterministic, self.requires_fit, self._skip_test, self.input_tags);
+            },
+            [](py::tuple t) {  // NOLINT: __setstate__
+                if (t.size() != 11) {
+                    throw std::runtime_error{ "Invalid Tags pickle state" };
+                }
+                // create a new C++ instance
+                Tags tags;
+                tags.estimator_type = t[0].cast<std::optional<std::string>>();
+                tags.target_tags = t[1].cast<TargetTags>();
+                tags.transformer_tags = t[2].cast<std::optional<TransformerTags>>();
+                tags.classifier_tags = t[3].cast<std::optional<ClassifierTags>>();
+                tags.regressor_tags = t[4].cast<std::optional<RegressorTags>>();
+                tags.array_api_support = t[5].cast<bool>();
+                tags.no_validation = t[6].cast<bool>();
+                tags.non_deterministic = t[7].cast<bool>();
+                tags.requires_fit = t[8].cast<bool>();
+                tags._skip_test = t[9].cast<bool>();
+                tags.input_tags = t[10].cast<InputTags>();
+                return tags;
+            }
+            )
+             // clang-format on
+        );
 }
