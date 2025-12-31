a45e1cbda9bb2a86bdb1e51e771f5b0aec971d79
Marcel Breyer <marcel.breyer@ipvs.uni-stuttgart.de>
2025-12-31 12:36:51 +0100

    Improve bounce checking ifs.

--- include/plssvm/backends/CUDA/kernel/cg_explicit/blas.cuh
+++ include/plssvm/backends/CUDA/kernel/cg_explicit/blas.cuh
@@ -49,7 +49,7 @@ __global__ void device_kernel_symm(const std::size_t num_rows, const std::size_t
     const auto global_j_idx = device_row_offset + device_global_j_idx;
 
     // be sure to not perform out-of-bounds accesses
-    if (global_i_idx < num_rhs && device_global_j_idx < device_num_rows) {
+    if (global_i_idx < num_rhs && global_j_idx < num_rows && device_global_j_idx < device_num_rows) {
         real_type temp{ 0.0 };
 
         // iterate over all values
@@ -101,7 +101,7 @@ __global__ void device_kernel_symm_mirror(const std::size_t num_rows, const std:
     const auto global_j_idx = device_row_offset + device_num_rows + partial_global_j_idx;
 
     // be sure to not perform out-of-bounds accesses
-    if (global_i_idx < num_rhs && partial_global_j_idx < num_mirror_rows && global_j_idx < num_rows) {
+    if (global_i_idx < num_rhs && global_j_idx < num_rows && partial_global_j_idx < num_mirror_rows) {
         real_type temp{ 0.0 };
 
         // iterate over all values
--- include/plssvm/backends/SYCL/kernel/cg_explicit/work_group/blas.hpp
+++ include/plssvm/backends/SYCL/kernel/cg_explicit/work_group/blas.hpp
@@ -77,7 +77,7 @@ class device_kernel_symm {
         const auto global_j_idx = device_row_offset_ + device_global_j_idx;
 
         // be sure to not perform out-of-bounds accesses
-        if (global_i_idx < num_rhs_ && device_global_j_idx < device_num_rows_) {
+        if (global_i_idx < num_rhs_ && global_j_idx < num_rows_ && device_global_j_idx < device_num_rows_) {
             real_type temp{ 0.0 };
 
             // iterate over all values
@@ -171,7 +171,7 @@ class device_kernel_symm_mirror {
         const auto global_j_idx = device_row_offset_ + device_num_rows_ + partial_global_j_idx;
 
         // be sure to not perform out-of-bounds accesses
-        if (global_i_idx < num_rhs_ && partial_global_j_idx < num_mirror_rows_ && global_j_idx < num_rows_) {
+        if (global_i_idx < num_rhs_ && global_j_idx < num_rows_ && partial_global_j_idx < num_mirror_rows_) {
             real_type temp{ 0.0 };
 
             // iterate over all values
